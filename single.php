7b5c52cbc0eca5083be715ffde86772115fb36cf
Tim <dev@tim.ph>
2016-03-19 00:28:45 -0400

    adding required wp templates.

---
+++ single.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @package BoldGrid
+ */
+
+?>
+<div class="container">
+	<?php get_template_part( 'templates/content-single', get_post_type() );?>
+</div>
