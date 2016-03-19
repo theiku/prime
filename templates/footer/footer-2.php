ecffd9c0102f47d08673cea26fe3d353120035a7
Tim <dev@tim.ph>
2016-03-19 01:02:28 -0400

    adding containers on footer templates

--- templates/footer/footer-2.php
+++ templates/footer/footer-2.php
@@ -1,12 +1,14 @@
 <footer id="colophon" class="site-footer" role="contentinfo" <?php BoldGrid_Framework_Schema::footer( true ); ?>>
-	<?php do_action( 'boldgrid_footer_top' ); ?>
-	<div class="row">
-		<div class="col-md-6">
-			<?php do_action( 'boldgrid_menu_footer_center' ); ?>
-		</div>
-		<div class="col-md-6">
-			<?php do_action( 'boldgrid_display_attribution_links' ); ?>
-		</div><!-- .attribution .col -->
-	</div><!-- .row -->
-	<?php do_action( 'boldgrid_footer_bottom' ); ?>
+	<div class="container">
+		<?php do_action( 'boldgrid_footer_top' ); ?>
+		<div class="row">
+			<div class="col-md-6">
+				<?php do_action( 'boldgrid_menu_footer_center' ); ?>
+			</div>
+			<div class="col-md-6">
+				<?php do_action( 'boldgrid_display_attribution_links' ); ?>
+			</div><!-- .attribution .col -->
+		</div><!-- .row -->
+		<?php do_action( 'boldgrid_footer_bottom' ); ?>
+	</div>
 </footer><!-- #colophon -->
