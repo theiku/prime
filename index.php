d8eb74510f78c9b99302cee39165eafbb0c0d95c
Tim <dev@tim.ph>
2016-03-19 00:59:24 -0400

    adding index.php template and stylesheets.

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The core template of a BoldGrid theme.  If there's no home template
+ * then this template gets used.  One of two required WordPress files.
+ *
+ * @since 2.0
+ */
+
+if ( ! have_posts() ) :
+	get_template_part( 'templates/content', 'none' );
+endif;
+
+while ( have_posts() ) : the_post();
+	get_template_part( 'templates/content', get_post_type() !== 'post' ? get_post_type() : get_post_format() );
+endwhile;
+
+the_posts_navigation();
+?>
