35706f6a3fc13a6376acb159fbd3cdc8fd673c9a
Tim <dev@tim.ph>
2016-03-19 17:38:21 -0400

    moving duplicate code to template.

--- 404.php
+++ 404.php
@@ -8,30 +8,7 @@
 				<?php get_search_form(); ?>
 			</div>
 		</div>
-		<div class="row">
-			<div class="col-md-6 search-posts">
-				<h3><?php _e( 'Recent Articles' ); ?></h3>
-				<ul class="list-group">
-					<?php
-						$args = array( 'numberposts' => '10', 'post_status' => 'publish' );
-						$recent_posts = wp_get_recent_posts( $args );
-						foreach( $recent_posts as $recent ) {
-							echo '<li class="list-group-item"><a href="' . get_permalink( $recent["ID"] ) . '">' . $recent["post_title"] . '</a></li>';
-						}
-					?>
-				</ul>
-			</div> <!-- .search-posts -->
-			<div class="col-md-6 search-pages">
-				<h3><?php _e( 'Page Sitemap' ); ?></h3>
-				<ul class="list-group">
-				<?php $args = array( 'numberposts' => '10', 'sort_column' => 'menu_order' );
-					$posts = get_pages( $args );
-						foreach( $posts as $post ) {
-							echo '<li class="list-group-item"><a href="' . get_permalink( $post->ID ) . '">' . $post->post_title . '</a></li>';
-						} ?>
-				</ul>
-			</div> <!-- .search-pages -->
-		</div> <!-- .row -->
+		<?php get_template_part( 'templates/recent-entries' ); ?>
 	</div> <!-- .container -->
 </div> <!-- .jumbotron -->
 <?php do_action( '404_after' ); ?>
--- search.php
+++ search.php
@@ -49,29 +49,6 @@ $results_count = $wp_query->found_posts;
 			<p><?php _e( 'Sorry. We couldn&rsquo;t find anything for that search. View one of our site&rsquo;s pages or a recent article below.' ); ?></p>
 		</div><!-- .col-md-12 -->
 	</div> <!-- .row -->
-		<div class="row">
-			<div class="col-md-6 search-posts">
-				<h3><?php _e( 'Recent Articles' ); ?></h3>
-				<ul class="list-group">
-					<?php
-						$args = array( 'numberposts' => '10', 'post_status' => 'publish' );
-						$recent_posts = wp_get_recent_posts( $args );
-						foreach( $recent_posts as $recent ) {
-							echo '<li class="list-group-item"><a href="' . get_permalink( $recent["ID"] ) . '">' . $recent["post_title"] . '</a></li>';
-						}
-					?>
-				</ul>
-			</div> <!-- .search-posts -->
-			<div class="col-md-6 search-pages">
-				<h3><?php _e( 'Page Sitemap' ); ?></h3>
-				<ul class="list-group">
-				<?php $args = array( 'numberposts' => '10', 'sort_column' => 'menu_order' );
-					$posts = get_pages( $args );
-						foreach( $posts as $post ) {
-							echo '<li class="list-group-item"><a href="' . get_permalink( $post->ID ) . '">' . $post->post_title . '</a></li>';
-						} ?>
-				</ul>
-			</div> <!-- .search-pages -->
-		</div> <!-- .row -->
+	<?php get_template_part( 'templates/recent-entries' ); ?>
 	<?php endif; ?>
 </div><!-- .container -->
