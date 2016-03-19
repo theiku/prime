093232406d7e428373ab192ca59ef9ed394c9875
Tim <dev@tim.ph>
2016-03-19 09:28:03 -0400

    adding header template

--- templates/header/header.php
+++ templates/header/header.php
@@ -5,32 +5,32 @@
 		<?php do_action( 'boldgrid_header_top' ); ?>
 		<?php dynamic_sidebar( 'boldgrid-widget-2' ); ?>
 		<div class="row">
-			<div class="col-md-6">
+			<div class="col-md-12">
 				<div class="site-branding">
 					<?php do_action( 'boldgrid_site_title' ); ?>
 					<?php do_action( 'boldgrid_print_tagline' ); ?>
 				</div>
 			</div>
-			<div class="col-md-6">
+		</div>
+		<div class"row">
+			<div class="col-md-12">
 				<?php do_action( 'boldgrid_menu_social' ); ?>
 			</div>
 		</div>
-		<div class="row">
-			<div class="col-md-12">
-				<nav id="site-navigation" class="navbar navbar-default col-md-12" role="navigation">
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#primary-navbar">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-					</div><!-- .navbar-header -->
-					<?php do_action( 'boldgrid_menu_primary' ); ?>
-				</nav><!-- #site-navigation -->
-			</div>
-		</div>
-		<?php do_action( 'boldgrid_header_bottom' ); ?>
 	</div><!-- .container -->
+	<nav id="site-navigation" class="navbar navbar-default col-md-12" role="navigation">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#primary-navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+		</div><!-- .navbar-header -->
+		<div class="container">
+			<?php do_action( 'boldgrid_menu_primary' ); ?>
+		</div>
+	</nav><!-- #site-navigation -->
 	<?php do_action( 'boldgrid_menu_tertiary' ); ?>
+	<?php do_action( 'boldgrid_header_bottom' ); ?>
 </header><!-- #masthead -->
