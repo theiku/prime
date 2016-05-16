66b060215e8ebd9410a97b12600d5f2dc3b622e7
Rafael Ramos <rramo012@gmail.com>
2016-05-16 14:58:10 -0400

    Wrapping entry header in contianer

--- templates/content-home.php
+++ templates/content-home.php
@@ -21,7 +21,9 @@ if ( $cta === 'all-pages' || $cta === 'home-only' ) {
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+		<div class='container'>
+			<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+		</div>
 	</header><!-- .entry-header -->
 	<div class="entry-content">
 		<?php the_content(); ?>
--- templates/content-page.php
+++ templates/content-page.php
@@ -20,7 +20,9 @@ if ( $cta === 'all-pages' ) {
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+		<div class='container'>
+			<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+		</div>
 	</header><!-- .entry-header -->
 	<div class="entry-content">
 		<?php the_content(); ?>
