f10d58e65412abd045474efc756a2b9f774ff730
Rafael Ramos <rramo012@gmail.com>
2016-05-24 12:20:56 -0400

    Adding Gereric header

---
+++ templates/header/header-generic.php
@@ -0,0 +1,43 @@
+<?php
+// Get the theme configs.
+global $boldgrid_theme_framework;
+$configs = $boldgrid_theme_framework->get_configs();
+?>
+
+<header id="masthead" class="boldgrid-section-group <?php echo basename(__FILE__, '.php'); ?>" role="banner" <?php BoldGrid_Framework_Schema::header( true ); ?>>
+
+	<?php do_action( 'boldgrid_header_top' ); ?>
+
+		<div class='boldgrid-section'>
+			<div class="container">
+				<div class='row'>
+					<div class='col-md-12'><?php do_action( 'boldgrid-theme-location', 'header', '1' ); ?></div>
+				</div>
+				<div class='row'>
+					<div class='col-md-4'><?php do_action( 'boldgrid-theme-location', 'header', '2' ); ?></div>
+					<div class='col-md-4'><?php do_action( 'boldgrid-theme-location', 'header', '3' ); ?></div>
+					<div class='col-md-4'><?php do_action( 'boldgrid-theme-location', 'header', '4' ); ?></div>
+				</div>
+				<div class='row'>
+					<div class='col-md-12'><?php do_action( 'boldgrid-theme-location', 'header', '5' ); ?></div>
+				</div>
+				<div class='row'>
+					<div class='col-md-6'><?php do_action( 'boldgrid-theme-location', 'header', '6' ); ?></div>
+					<div class='col-md-6'><?php do_action( 'boldgrid-theme-location', 'header', '7' ); ?></div>
+				</div>
+				<div class='row'>
+					<div class='col-md-12'><?php do_action( 'boldgrid-theme-location', 'header', '8' ); ?></div>
+				</div>
+				<div class='row'>
+					<div class='col-md-6'><?php do_action( 'boldgrid-theme-location', 'header', '9' ); ?></div>
+					<div class='col-md-6'><?php do_action( 'boldgrid-theme-location', 'header', '10' ); ?></div>
+				</div>
+				<div class='row'>
+					<div class='col-md-12'><?php do_action( 'boldgrid-theme-location', 'header', '11' ); ?></div>
+				</div>
+			</div><!-- .container -->
+		</div><!-- .section -->
+
+	<?php do_action( 'boldgrid_header_bottom' ); ?>
+
+</header><!-- #masthead -->
