e6277d68b05f79afb12e71284f7e1e7652a8d266
Tim <dev@tim.ph>
2016-05-26 18:40:46 -0400

    add generic footer template.

---
+++ templates/footer/footer-generic.php
@@ -0,0 +1,33 @@
+<footer id="colophon" role="contentinfo" <?php BoldGrid_Framework_Schema::footer( true ); ?>>
+	<?php do_action( 'boldgrid_footer_top' ); ?>
+		<div class='boldgrid-section'>
+			<div class="container">
+				<div class='row'>
+					<div class='col-md-12'><?php do_action( 'boldgrid-theme-location', 'footer', '1' ); ?></div>
+				</div>
+				<div class='row'>
+					<div class='col-md-4'><?php do_action( 'boldgrid-theme-location', 'footer', '2' ); ?></div>
+					<div class='col-md-4'><?php do_action( 'boldgrid-theme-location', 'footer', '3' ); ?></div>
+					<div class='col-md-4'><?php do_action( 'boldgrid-theme-location', 'footer', '4' ); ?></div>
+				</div>
+				<div class='row'>
+					<div class='col-md-12'><?php do_action( 'boldgrid-theme-location', 'footer', '5' ); ?></div>
+				</div>
+				<div class='row'>
+					<div class='col-md-7'><?php do_action( 'boldgrid-theme-location', 'footer', '6' ); ?></div>
+					<div class='col-md-5'><?php do_action( 'boldgrid-theme-location', 'footer', '7' ); ?></div>
+				</div>
+				<div class='row'>
+					<div class='col-md-12'><?php do_action( 'boldgrid-theme-location', 'footer', '8' ); ?></div>
+				</div>
+				<div class='row'>
+					<div class='col-md-6'><?php do_action( 'boldgrid-theme-location', 'footer', '9' ); ?></div>
+					<div class='col-md-6'><?php do_action( 'boldgrid-theme-location', 'footer', '10' ); ?></div>
+				</div>
+				<div class='row'>
+					<div class='col-md-12'><?php do_action( 'boldgrid-theme-location', 'footer', '11' ); ?></div>
+				</div>
+			</div><!-- .container -->
+		</div><!-- .section -->
+	<?php do_action( 'boldgrid_footer_bottom' ); ?>
+</footer><!-- #colophon -->
