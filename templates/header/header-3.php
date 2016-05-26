c91ed00f984f44bd63f85ef7ed8e5ec67ebf95d0
Tim <dev@tim.ph>
2016-05-26 12:17:19 -0400

    Update header-3 template.

--- templates/header/header-3.php
+++ templates/header/header-3.php
@@ -1,33 +1,72 @@
-<header id="masthead" class="site-header <?php echo esc_attr( basename(__FILE__, '.php' ) ); ?>" role="banner" <?php BoldGrid_Framework_Schema::header( true ); ?>>
-<?php BoldGrid::skip_link(  ); ?>
+<?php
+/**
+ * Header Template 3
+ *
+ * This is a BoldGrid Theme Header Template.
+ *
+ * @package BoldGrid
+ */
+
+// Get the theme configs.
+global $boldgrid_theme_framework;
+$configs = $boldgrid_theme_framework->get_configs();
+?>
+
+<header id="masthead" class="site-header" role="banner" <?php BoldGrid_Framework_Schema::header( true ); ?>>
+	<?php BoldGrid::skip_link(); ?>
+	<?php do_action( 'boldgrid_header_top' ); ?>
 	<div class="container">
-		<div class="col-md-12">
-			<div class="alignright"><?php do_action( 'boldgrid_menu_tertiary' ); ?></div>
-		</div><!-- .col -->
-		<div class="col-md-12">
-			<?php do_action( 'boldgrid_menu_secondary' ); ?>
-		</div><!-- .col -->
-		<?php do_action( 'boldgrid_header_top' ); ?>
-		<div class="site-branding">
-			<?php do_action( 'boldgrid_site_title' ); ?>
-			<?php do_action( 'boldgrid_print_tagline' ); ?>
-		</div><!-- .site-branding -->
-		<nav id="site-navigation" class="navbar navbar-default" role="navigation">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#primary-navbar">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-			</div><!-- .navbar-header -->
-			<?php do_action('boldgrid_menu_primary'); ?>
-		</nav><!-- #site-navigation -->
-		<?php do_action( 'boldgrid_header_bottom' ); ?>
-		<div class="row call-to-action-wrapper">
+		<div class="row">
 			<div class="col-md-12">
-				<?php dynamic_sidebar( 'boldgrid-widget-2' ); ?>
+				<?php do_action( 'boldgrid_menu_secondary' ); ?>
 			</div><!-- .col -->
 		</div><!-- .row -->
+		<div class="row">
+			<div class="col-sm-4">
+				<?php do_action( 'boldgrid_site_identity' ); ?>
+			</div>
+			<div class="col-sm-8">
+				<div class="row">
+					<div class="col-md-12">
+						<?php do_action( 'boldgrid_menu_social' ); ?>
+					</div>
+				</div>
+				<?php if ( has_nav_menu( 'primary' ) ) : ?>
+					<div class="row">
+						<div class="col-md-12">
+							<nav id="site-navigation" class="navbar navbar-default" role="navigation">
+								<div class="container">
+									<div class="navbar-header">
+										<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#primary-navbar">
+											<span class="sr-only">Toggle navigation</span>
+											<span class="icon-bar"></span>
+											<span class="icon-bar"></span>
+											<span class="icon-bar"></span>
+										</button>
+									</div><!-- .navbar-header -->
+									<div class="navbar-collapse collapse" id="primary-navbar">
+										<?php do_action( 'boldgrid_menu_primary' ); ?>
+										<?php if ( true === $configs['template']['navbar-search-form'] ) : ?>
+											<?php get_template_part( 'templates/header/search' ); ?>
+										<?php endif; ?>
+									</div>
+								</div>
+							</nav><!-- #site-navigation -->
+						</div><!-- .col -->
+					</div><!-- .row -->
+				<?php endif; ?>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<?php dynamic_sidebar( 'boldgrid-widget-2' ); ?>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<?php do_action( 'boldgrid_menu_tertiary' ); ?>
+			</div>
+		</div>
 	</div><!-- .container -->
+	<?php do_action( 'boldgrid_header_bottom' ); ?>
 </header><!-- #masthead -->
