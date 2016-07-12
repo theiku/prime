f87e1d910a38d0e5c50796c79a5ae58271e41746
Rafael Ramos <rramo012@gmail.com>
2016-07-12 15:57:00 -0400

    Fixing empty

--- base.php
+++ base.php
@@ -22,7 +22,7 @@ $configs = $boldgrid_theme_framework->get_configs();
 		<?php do_action( 'boldgrid_header_after' ); ?>
 		<?php do_action( 'boldgrid_content_before' ); ?>
 		<div class="site-content" role="document">
-			<main class="main <?php echo BoldGrid::print_container_class()?><?php echo ! BoldGrid::display_sidebar( ) ? : ' col-md-8'; ?>">
+			<main class="main <?php echo BoldGrid::print_container_class()?><?php echo ! BoldGrid::display_sidebar( ) ? '' : ' col-md-8'; ?>">
 				<?php include Boldgrid_Framework_Wrapper::boldgrid_template_path(); ?>
 			</main><!-- /.main -->
 			<?php if ( BoldGrid::display_sidebar( ) ) : ?>
