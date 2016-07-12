409b6a60eaf9c856afb3e0e390623edabb43eeed
Tim <dev@tim.ph>
2016-07-12 18:24:06 -0400

    remove container from singular

--- single.php
+++ single.php
@@ -6,6 +6,5 @@
  */
 
 ?>
-<div class="container">
-	<?php get_template_part( 'templates/content-single', get_post_type() );?>
-</div>
+
+<?php get_template_part( 'templates/content-single', get_post_type() );?>
