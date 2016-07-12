b0b2605dfd9447d19068cefa4cffa1dfaed2cb25
Tim <dev@tim.ph>
2016-07-12 15:38:11 -0400

    formatting.

--- functions.php
+++ functions.php
@@ -3,5 +3,5 @@
  * Include the BoldGrid Theme Framework
  */
 locate_template( '/inc/boldgrid-theme-framework-config/config.php', true, true );
-require_once get_template_directory(  ) . '/inc/boldgrid-theme-framework/boldgrid-theme-framework.php';
+require_once get_template_directory() . '/inc/boldgrid-theme-framework/boldgrid-theme-framework.php';
 ?>
