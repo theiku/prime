177e65b4657df2697fc1689147af8a53decd9373
Tim <dev@tim.ph>
2016-07-19 17:06:41 -0400

    add global footer config to generic footer.

--- templates/footer/footer-generic.php
+++ templates/footer/footer-generic.php
@@ -7,7 +7,7 @@ $configs = $boldgrid_theme_framework->get_configs();
 <footer id="colophon" role="contentinfo" <?php BoldGrid_Framework_Schema::footer( true ); ?>>
 	<?php do_action( 'boldgrid_footer_top' ); ?>
 		<div class='boldgrid-section'>
-			<div class="bgtfw <?php echo BoldGrid::print_container_class( 'header' )?>">
+			<div class="bgtfw <?php echo $configs['template']['pages']['global']['footer']; ?>">
 				<div class='row footer-1'>
 					<div class='col-md-12 footer-1'><?php do_action( 'boldgrid-theme-location', 'footer', '1' ); ?></div>
 				</div>
