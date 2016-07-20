a0f0aefc3f6286f587366ba77a25724cf7da7e6c
Tim <dev@tim.ph>
2016-07-20 11:34:59 -0400

    adding call to action container config

--- templates/call-to-action.php
+++ templates/call-to-action.php
@@ -1,1 +1,11 @@
-<?php dynamic_sidebar( 'boldgrid-widget-1' ); ?>
+<?php
+// Get the theme configs.
+global $boldgrid_theme_framework;
+$configs = $boldgrid_theme_framework->get_configs();
+
+// Display the call to action widget area if configs are set.
+?>
+<div class="bgtfw-cta <?php echo $configs['template']['pages']['global']['call-to-action']; ?>">
+	<?php dynamic_sidebar( 'boldgrid-widget-1' );?>
+</div>
+?>
