24abd4ce099a05d365550484e1d424bbdc30b9a9
Tim <dev@tim.ph>
2016-07-21 16:14:41 -0400

    check configs for cta widget

--- templates/call-to-action.php
+++ templates/call-to-action.php
@@ -2,9 +2,18 @@
 // Get the theme configs.
 global $boldgrid_theme_framework;
 $configs = $boldgrid_theme_framework->get_configs();
-
+$widgets = $configs['widget']['widget_instances'];
 // Display the call to action widget area if configs are set.
 ?>
 <div class="bgtfw-cta <?php echo $configs['template']['pages']['global']['call-to-action']; ?>">
-	<?php dynamic_sidebar( 'boldgrid-widget-1' );?>
+	<?php
+		$location = 'boldgrid-widget-1';
+		foreach( $widgets as $widget => $widget_configs ) {
+			if ( isset( $widget_configs[0]['title'] ) && 'Call To Action' === $widget_configs[0]['title'] ) {
+				$location = $widget;
+				break;
+			}
+		}
+		dynamic_sidebar( $location );
+	?>
 </div>
