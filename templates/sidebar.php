9b9422f3c97bf093fb041e4bed86737d6e3b06a4
Tim <dev@tim.ph>
2016-08-09 15:38:28 -0400

    adding empty widget message

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -6,3 +6,9 @@
  */
 
 dynamic_sidebar( 'sidebar-1' ); ?>
+<?php if ( ! is_active_sidebar( 'sidebar-1' ) && current_user_can ( 'edit_pages' ) ) : ?>
+	<div class="empty-sidebar-message">
+		<h2>Empty Sidebar</h2>
+		<p>This sidebar doesn't have any widgets assigned to it.  <a href="">Add widgets here.</a></p>
+	</div>
+<?php endif;
