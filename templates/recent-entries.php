4cefed870173b8f0d8d7ade1f46e6edde9ee7e7a
Tim <dev@tim.ph>
2016-10-11 16:01:47 -0400

    fixing 404 info

--- templates/recent-entries.php
+++ templates/recent-entries.php
@@ -1,28 +1,41 @@
-<div class="row">
-	<div class="col-md-6 search-posts">
-		<h3><?php _e( 'Recent Articles' ); ?></h3>
-		<ul class="list-group">
-			<?php
-				$args = array( 'numberposts' => '10', 'post_status' => 'publish' );
-				$recent_posts = wp_get_recent_posts( $args );
-				foreach( $recent_posts as $recent ) {
-					if ( $recent['post_title'] ) {
-						echo '<li class="list-group-item"><a href="' . get_permalink( $recent['ID'] ) . '">' . $recent['post_title'] . '</a></li>';
-					}
-				}
-			?>
-		</ul>
-	</div> <!-- .search-posts -->
-	<div class="col-md-6 search-pages">
-		<h3><?php _e( 'Page Sitemap' ); ?></h3>
-		<ul class="list-group">
-		<?php $args = array( 'numberposts' => '10', 'sort_column' => 'menu_order' );
-			$posts = get_pages( $args );
-				foreach( $posts as $post ) {
-					if ( $post->post_title ) {
-						echo '<li class="list-group-item"><a href="' . get_permalink( $post->ID ) . '">' . $post->post_title . '</a></li>';
-					}
-				} ?>
-		</ul>
-	</div> <!-- .search-pages -->
-</div> <!-- .row -->
+<?php
+	$recent_posts = wp_get_recent_posts( array( 'numberposts' => '10', 'post_status' => 'publish' ) );
+	$pages = get_pages( array( 'sort_column' => 'menu_order' ) );
+?>
+
+<?php if ( $recent_posts || $pages ) : ?>
+	<div class="row">
+		<?php if ( $recent_posts ) : ?>
+			<div class="<?php echo ! $pages ? 'col-md-12' : 'col-md-6'; ?> search-posts">
+				<h3><?php _e( 'Recent Articles' ); ?></h3>
+				<ul class="list-group">
+					<?php
+						$args = array( 'numberposts' => '10', 'post_status' => 'publish' );
+						$recent_posts = wp_get_recent_posts( $args );
+						foreach( $recent_posts as $recent ) {
+							if ( $recent['post_title'] ) {
+								echo '<li class="list-group-item"><a href="' . get_permalink( $recent['ID'] ) . '">' . $recent['post_title'] . '</a></li>';
+							}
+						}
+					?>
+				</ul>
+			</div> <!-- .search-posts -->
+		<?php endif; ?>
+		<?php if ( $pages ) : ?>
+			<div class="<?php echo ! $recent_posts ? 'col-md-12' : 'col-md-6'; ?> search-pages">
+				<h3><?php _e( 'Page Sitemap' ); ?></h3>
+				<ul class="list-group">
+					<?php
+						$count = 0;
+						foreach( $pages as $page ) {
+							if ( $page->post_title && $count < 10 ) {
+								echo '<li class="list-group-item"><a href="' . get_permalink( $page->ID ) . '">' . $page->post_title . '</a></li>';
+							}
+							$count++;
+						}
+					?>
+				</ul>
+			</div> <!-- .search-pages -->
+		<?php endif; ?>
+	</div> <!-- .row -->
+<?php endif; ?>
