0e1d7f1d0c7d655fe4dfd32aab4c0c87352c32b3
Tim <dev@tim.ph>
2016-12-20 16:39:42 -0500

    fixing code standards

--- base.php
+++ base.php
@@ -23,7 +23,7 @@ $configs = $boldgrid_theme_framework->get_configs();
 		<?php do_action( 'boldgrid_header_after' ); ?>
 		<?php do_action( 'boldgrid_content_before' ); ?>
 		<div class="site-content" role="document">
-			<main class="main <?php echo BoldGrid::print_container_class('blog')?>">
+			<main class="main <?php echo BoldGrid::print_container_class( 'blog' ); ?>">
 				<div class="row">
 					<div class="<?php echo ! BoldGrid::display_sidebar( ) ? '' : 'col-md-9'; ?>">
 						<?php include Boldgrid_Framework_Wrapper::boldgrid_template_path(); ?>
