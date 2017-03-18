4e9d795083dea33a173094de79b4390373e2dd97
Tim <dev@tim.ph>
2017-03-17 20:13:53 -0400

    updating add to cart button to be primary button style

--- woocommerce/single-product/add-to-cart/external.php
+++ woocommerce/single-product/add-to-cart/external.php
@@ -25,7 +25,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
 
 <p class="cart">
-	<a href="<?php echo esc_url( $product_url ); ?>" rel="nofollow" class="single_add_to_cart_button btn button-secondary"><?php echo esc_html( $button_text ); ?></a>
+	<a href="<?php echo esc_url( $product_url ); ?>" rel="nofollow" class="single_add_to_cart_button btn button-primary"><?php echo esc_html( $button_text ); ?></a>
 </p>
 
 <?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
--- woocommerce/single-product/add-to-cart/simple.php
+++ woocommerce/single-product/add-to-cart/simple.php
@@ -67,7 +67,7 @@ if ( ! $product->is_purchasable() ) {
 
 	 	<input type="hidden" name="add-to-cart" value="<?php echo esc_attr( $product->id ); ?>" />
 
-	 	<button type="submit" class="single_add_to_cart_button btn button-secondary"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+	 	<button type="submit" class="single_add_to_cart_button btn button-primary"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
 
 		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
 	</form>
