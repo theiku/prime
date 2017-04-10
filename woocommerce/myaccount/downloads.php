7bc68510280af059f484b952b6a92f0ec37bee46
Tim <dev@tim.ph>
2017-04-10 13:54:23 -0400

    updating downloads template to wc 3.0

--- woocommerce/myaccount/downloads.php
+++ woocommerce/myaccount/downloads.php
@@ -15,7 +15,7 @@
  * @see 	https://docs.woocommerce.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
- * @version 2.6.0
+ * @version 3.0.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -43,47 +43,48 @@ do_action( 'woocommerce_before_account_downloads', $has_downloads ); ?>
 			<tr>
 				<?php foreach ( wc_get_account_downloads_columns() as $column_id => $column_name ) : ?>
 					<td class="<?php echo esc_attr( $column_id ); ?>" data-title="<?php echo esc_attr( $column_name ); ?>">
-						<?php if ( has_action( 'woocommerce_account_downloads_column_' . $column_id ) ) : ?>
-							<?php do_action( 'woocommerce_account_downloads_column_' . $column_id, $download ); ?>
-
-						<?php elseif ( 'download-file' === $column_id ) : ?>
-							<a href="<?php echo esc_url( get_permalink( $download['product_id'] ) ); ?>">
-								<?php echo esc_html( $download['download_name'] ); ?>
-							</a>
-
-						<?php elseif ( 'download-remaining' === $column_id ) : ?>
-							<?php
-								if ( is_numeric( $download['downloads_remaining'] ) ) {
-									echo esc_html( $download['downloads_remaining'] );
-								} else {
-									_e( '&infin;', 'woocommerce' );
-								}
-							?>
-
-						<?php elseif ( 'download-expires' === $column_id ) : ?>
-							<?php if ( ! empty( $download['access_expires'] ) ) : ?>
-								<time datetime="<?php echo date( 'Y-m-d', strtotime( $download['access_expires'] ) ); ?>" title="<?php echo esc_attr( strtotime( $download['access_expires'] ) ); ?>"><?php echo date_i18n( get_option( 'date_format' ), strtotime( $download['access_expires'] ) ); ?></time>
-							<?php else : ?>
-								<?php _e( 'Never', 'woocommerce' ); ?>
-							<?php endif; ?>
-
-						<?php elseif ( 'download-actions' === $column_id ) : ?>
-							<?php
-								$actions = array(
-									'download'  => array(
-										'url'  => $download['download_url'],
-										'name' => __( 'Download', 'woocommerce' )
-									)
-								);
-
-								if ( $actions = apply_filters( 'woocommerce_account_download_actions', $actions, $download ) ) {
-									foreach ( $actions as $key => $action ) {
-										echo '<a href="' . esc_url( $action['url'] ) . '" class="button woocommerce-Button ' . sanitize_html_class( $key ) . '">' . esc_html( $action['name'] ) . '</a>';
-									}
-								}
-							?>
-
-						<?php endif; ?>
+						<?php if ( has_action( 'woocommerce_account_downloads_column_' . $column_id ) ) {
+							do_action( 'woocommerce_account_downloads_column_' . $column_id, $download );
+						} else {
+							switch ( $column_id ) {
+								case 'download-product' : ?>
+									<a href="<?php echo esc_url( get_permalink( $download['product_id'] ) ); ?>">
+										<?php echo esc_html( $download['product_name'] ); ?>
+									</a>
+									<?php break;
+								case 'download-file' : ?>
+									<a href="<?php echo esc_url( $download['download_url'] ); ?>" class="woocommerce-MyAccount-downloads-file">
+										<?php echo esc_html( $download['file']['name'] ); ?>
+									</a>
+									<?php break;
+								case 'download-remaining' :
+									echo is_numeric( $download['downloads_remaining'] ) ? esc_html( $download['downloads_remaining'] ) : __( '&infin;', 'woocommerce' );
+									break;
+								case 'download-expires' : ?>
+									<?php if ( ! empty( $download['access_expires'] ) ) : ?>
+										<time datetime="<?php echo date( 'Y-m-d', strtotime( $download['access_expires'] ) ); ?>" title="<?php echo esc_attr( strtotime( $download['access_expires'] ) ); ?>"><?php echo date_i18n( get_option( 'date_format' ), strtotime( $download['access_expires'] ) ); ?></time>
+									<?php else : ?>
+										<?php _e( 'Never', 'woocommerce' ); ?>
+									<?php endif; ?>
+									<?php break;
+								case 'download-actions' : ?>
+									<?php
+										$actions = array(
+											'download'  => array(
+												'url'  => $download['download_url'],
+												'name' => __( 'Download', 'woocommerce' ),
+											),
+										);
+										if ( $actions = apply_filters( 'woocommerce_account_download_actions', $actions, $download ) ) {
+											foreach ( $actions as $key => $action ) {
+												echo '<a href="' . esc_url( $action['url'] ) . '" class="woocommerce-Button btn button-primary ' . sanitize_html_class( $key ) . '">' . esc_html( $action['name'] ) . '</a>';
+											}
+										}
+									?>
+									<?php break;
+							}
+						}
+						?>
 					</td>
 				<?php endforeach; ?>
 			</tr>
