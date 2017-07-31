fb3c7f35f007ce42cb2409f836e9c53b6006bed0
Tim Elsass <timelsass@users.noreply.github.com>
2017-07-30 22:09:17 -0400

    Update entry-footer.php

--- templates/entry-footer.php
+++ templates/entry-footer.php
@@ -9,15 +9,5 @@
 ?>
 
 <div class="bgtfw <?php echo BoldGrid::print_container_class( 'entry-footer' )?>">
-<?php
-	edit_post_link(
-		sprintf(
-			/* translators: %s: Name of current post */
-			esc_html__( 'Edit %s', 'bgtfw' ),
-			the_title( '<span class="screen-reader-text">"', '"</span>', false )
-		),
-		'<i class="fa fa-pencil">',
-		'</i>'
-	);
-?>
+	<?php bgtfw_edit_post_link(); ?>
 </div>
