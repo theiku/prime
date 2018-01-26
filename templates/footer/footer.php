f7f09ea9b32840d327e41c525419ba438b9bd87a
Tim <dev@tim.ph>
2018-01-26 13:38:30 -0500

    adding template wrappers

--- templates/footer/footer.php
+++ templates/footer/footer.php
@@ -15,9 +15,17 @@ $configs = $boldgrid_theme_framework->get_configs();
 
 <footer id="colophon" <?php bgtfw_footer_class( array( basename( __FILE__, '.php' ), $configs['template']['pages']['global']['footer'] ) ); ?> role="contentinfo" <?php BoldGrid_Framework_Schema::footer( true ); ?>>
 	<?php do_action( 'boldgrid_footer_top' ); ?>
-	<?php do_action( 'boldgrid_menu_social' ); ?>
-	<?php do_action( 'boldgrid_menu_footer_center' ); ?>
-	<?php do_action( 'boldgrid_display_attribution_links' ); ?>
-	<?php do_action( 'boldgrid_display_contact_block' ); ?>
+	<div class="social-menu-wrapper">
+		<?php do_action( 'boldgrid_menu_social' ); ?>
+	</div>
+	<div class="footer-center-menu-wrapper">
+		<?php do_action( 'boldgrid_menu_footer_center' ); ?>
+	</div>
+	<div class="attribution-theme-mods-wrapper">
+		<?php do_action( 'boldgrid_display_attribution_links' ); ?>
+	</div>
+	<div class="contact-block-wrapper">
+		<?php do_action( 'boldgrid_display_contact_block' ); ?>
+	</div>
 	<?php do_action( 'boldgrid_footer_bottom' ); ?>
 </footer><!-- #colophon -->
