df73ae8816155af6cabbb7301a1133bfa6b5b97e
Tim <dev@tim.ph>
2018-02-09 13:41:55 -0500

    updating content template

--- templates/content.php
+++ templates/content.php
@@ -9,30 +9,36 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header <?php echo BoldGrid::print_container_class( 'entry-header' )?>">
-		<?php get_template_part( 'templates/entry-header' ); ?>
-		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php boldgrid_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
+	<header class="entry-header <?php echo BoldGrid::print_container_class( 'entry-header' )?>" <?php bgtfw_featured_img_bg( $post->ID ); ?>>
+		<div class="featured-imgage-header">
+			<?php get_template_part( 'templates/entry-header' ); ?>
+			<?php if ( 'post' == get_post_type() ) : ?>
+				<div class="entry-meta">
+					<?php boldgrid_posted_on(); ?>
+				</div><!-- .entry-meta -->
+			<?php endif; ?>
+		</div>
 	</header><!-- .entry-header -->
 	<div class="entry-content">
-		<?php if ( has_post_thumbnail() ) : ?>
-			<?php the_post_thumbnail(); ?>
-		<?php endif; ?>
-		<?php
-			/* translators: %s: Name of current post */
-			the_content( sprintf(
-				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'boldgrid' ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-		?>
-		<?php wp_link_pages( array(
-			'before' => '<div class="page-links">' . __( 'Pages:', 'bgtfw' ),
-			'after'  => '</div>',
-			)
-		); ?>
+	<?php
+		/* translators: %s: Name of current post */
+		$content = get_theme_mod( 'bgtfw_pages_blog_blog_page_layout_content', 'excerpt' );
+		if ( $content === 'excerpt' ) {
+			the_excerpt();
+		} else {
+			if ( get_theme_mod( 'bgtfw_pages_blog_blog_page_layout_featimg', false ) && has_post_thumbnail() ) : ?>
+				<div class="featured-image container">
+					<?php the_post_thumbnail(); ?>
+				</div>
+			<?php endif; ?>
+			<?php the_content( sprintf( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'boldgrid' ), the_title( '<span class="screen-reader-text">"', '"</span>', false ) ) );
+		}
+	?>
+	<?php wp_link_pages( array(
+		'before' => '<div class="page-links">' . __( 'Pages:', 'bgtfw' ),
+		'after'  => '</div>',
+		)
+	); ?>
 	</div><!-- .entry-content -->
 	<footer class="entry-footer">
 		<?php boldgrid_entry_footer(); ?>
