1cc5ac66330fdd272f62dceef2bdc74690904c0f
Joe C <joe@css-solutions.com>
2018-02-22 16:18:35 -0500

    BGTHEME-103: Added filter to enable the ClaimPremiumKey notice.

--- functions.php
+++ functions.php
@@ -7,3 +7,6 @@
 
 locate_template( '/inc/boldgrid-theme-framework-config/config.php', true, true );
 require_once get_template_directory() . '/inc/boldgrid-theme-framework/boldgrid-theme-framework.php';
+
+// Enable the ClaimPremiumKey notice.
+add_filter( 'Boldgrid\Library\Library\Notice\ClaimPremiumKey_enable', '__return_true' );
