97f6032a783df6f8efd73aa795ccbf7677ef61da
Tim <dev@tim.ph>
2018-02-27 15:27:20 -0500

    updating page template

--- templates/content-page.php
+++ templates/content-page.php
@@ -21,15 +21,13 @@ if ( 'all-pages' === $cta ) {
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php do_action( 'before_entry_title' ); ?>
-	<header class="entry-header <?php echo BoldGrid::print_container_class( 'entry-header' )?>">
+	<header class="entry-header">
 		<?php get_template_part( 'templates/entry-header' ); ?>
 	</header><!-- .entry-header -->
 	<?php do_action( 'after_entry_title' ); ?>
 	<div class="entry-content">
-		<div class="bgtfw <?php echo BoldGrid::print_container_class( 'entry-content' )?>">
-			<?php the_content(); ?>
-			<?php wp_link_pages( array( 'before' => '<nav class="page-links"><p>' . esc_html__( 'Pages:', 'bgtfw' ), 'after' => '</p></nav>' ) ); ?>
-		</div><!-- .bgtfw container -->
+		<?php the_content(); ?>
+		<?php wp_link_pages( array( 'before' => '<nav class="page-links"><p>' . esc_html__( 'Pages:', 'bgtfw' ), 'after' => '</p></nav>' ) ); ?>
 	</div><!-- .entry-content -->
 	<footer class="entry-footer">
 		<?php get_template_part( 'templates/entry-footer' ); ?>
