2bf2bd08c8271d6e64982fc4fd0263619e30c2dc
Tim <dev@tim.ph>
2018-03-30 16:15:12 -0400

    updating latest woocommerce changes

--- woocommerce/loop/pagination.php
+++ woocommerce/loop/pagination.php
@@ -13,7 +13,7 @@
  * @see 	    https://docs.woocommerce.com/document/template-structure/
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     2.2.2
+ * @version     3.3.1
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
