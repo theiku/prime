2aa3a6bf1770d77bccbf8222857dba43af9f8253
Tim <dev@tim.ph>
2018-04-11 15:20:47 -0400

    update var

--- inc/boldgrid-theme-framework-config/config.php
+++ inc/boldgrid-theme-framework-config/config.php
@@ -15,47 +15,47 @@ if ( ! function_exists( 'boldgrid_prime_framework_config' ) ) {
 	 *
 	 * @since 1.0.0
 	 */
-	function boldgrid_prime_framework_config( $boldgrid_framework_configs ) {
+	function boldgrid_prime_framework_config( $config ) {
 
 		// Disable old typography controls in favor of new ones.
-		$boldgrid_framework_configs['customizer-options']['typography']['controls']['main_text'] = false;
-		$boldgrid_framework_configs['customizer-options']['typography']['controls']['subheadings'] = false;
-		$boldgrid_framework_configs['customizer-options']['site-title']['site-title'] = false;
+		$config['customizer-options']['typography']['controls']['main_text'] = false;
+		$config['customizer-options']['typography']['controls']['subheadings'] = false;
+		$config['customizer-options']['site-title']['site-title'] = false;
 
 		// Waiting for all themes to opt in before removing switch.
 		// Enable typography controls in the customizer.
-		$boldgrid_framework_configs['customizer-options']['typography']['enabled'] = true;
+		$config['customizer-options']['typography']['enabled'] = true;
 
 		// Enable Sticky Footer.
-		$boldgrid_framework_configs['scripts']['boldgrid-sticky-footer'] = true;
+		$config['scripts']['boldgrid-sticky-footer'] = true;
 
 		// Waiting for all themes to opt in before removing switch.
 		// Remove the wrong attribution links from the footer.
-		$boldgrid_framework_configs['temp']['attribution_links'] = true;
+		$config['temp']['attribution_links'] = true;
 
 		// Waiting for all themes to opt in before removing switch.
 		// Enable the color palettes in the customizer.
-		$boldgrid_framework_configs['customizer-options']['colors']['enabled'] = true;
+		$config['customizer-options']['colors']['enabled'] = true;
 
 		// Waiting for all themes to opt in before removing switch.
 		// Enable Bootstrap SCSS Compiling.
-		$boldgrid_framework_configs['bootstrap-compile'] = true;
+		$config['bootstrap-compile'] = true;
 
 		// Waiting for all themes to opt in before removing switch.
 		// Turn on the parent theme template engine.
-		$boldgrid_framework_configs['boldgrid-parent-theme'] = true;
+		$config['boldgrid-parent-theme'] = true;
 
 		// Set Theme Name.
-		$boldgrid_framework_configs['theme_name'] = 'prime';
+		$config['theme_name'] = 'prime';
 
 		// This theme doesn't support a background image.
-		$boldgrid_framework_configs['customizer-options']['background']['defaults']['background_image'] = false;
+		$config['customizer-options']['background']['defaults']['background_image'] = false;
 
 		// Disable Call to Action Widget.
-		$boldgrid_framework_configs['template']['call-to-action'] = 'disabled';
+		$config['template']['call-to-action'] = 'disabled';
 
 		// Default Colors.
-		$boldgrid_framework_configs['customizer-options']['colors']['defaults'] = array(
+		$config['customizer-options']['colors']['defaults'] = array(
 			array(
 				'default' => true,
 				'format' => 'palette-primary',
@@ -70,13 +70,13 @@ if ( ! function_exists( 'boldgrid_prime_framework_config' ) ) {
 			),
 		);
 
-		$boldgrid_framework_configs['menu']['footer_menus'][] = 'social';
+		$config['menu']['footer_menus'][] = 'social';
 
 		// Add container to header.
-		$boldgrid_framework_configs['template']['header'] = 'container';
+		$config['template']['header'] = 'container';
 
 		// Configs above will override framework defaults.
-		return $boldgrid_framework_configs;
+		return $config;
 	}
 }
 add_filter( 'boldgrid_theme_framework_config', 'boldgrid_prime_framework_config' );
