fdbf3d1e0c513e3ec6198af32b47a0aed43426db
Tim Elsass <timelsass@users.noreply.github.com>
2018-05-07 19:49:19 -0400

    adding wrapper to #navi in header

--- templates/header/header.php
+++ templates/header/header.php
@@ -7,40 +7,40 @@
  * @package Prime
  */
 
-// Get the theme configs.
-global $boldgrid_theme_framework;
-$configs = $boldgrid_theme_framework->get_configs();
 ?>
 
 <header id="masthead" <?php bgtfw_header_class( basename( __FILE__, '.php' ) ); ?> role="banner" <?php BoldGrid_Framework_Schema::header( true ); ?>>
 	<div class="custom-header-media">
 		<?php the_custom_header_markup(); ?>
 	</div>
-	<div id="navi" <?php bgtfw_inner_header_class() ?>>
+	<div id="navi-wrap" <?php bgtfw_inner_header_class() ?>>
+		<?php do_action( 'boldgrid_menu_secondary' ) ?>
+		<div id="navi">
 
 		<?php
 
-		$image_attributes = wp_get_attachment_image_src( absint( get_theme_mod( 'boldgrid_logo_setting' ) ), 'full' );
-		$alt_tag = get_post_meta( get_theme_mod( 'boldgrid_logo_setting' ), '_wp_attachment_image_alt', true );
+			$image_attributes = wp_get_attachment_image_src( absint( get_theme_mod( 'boldgrid_logo_setting' ) ), 'full' );
+			$alt_tag = get_post_meta( get_theme_mod( 'boldgrid_logo_setting' ), '_wp_attachment_image_alt', true );
 
-		$alt = '';
-		if ( ! empty( $alt_tag ) ) {
-			$alt = 'alt="' . $alt_tag . '"';
-		}
+			$alt = '';
+			if ( ! empty( $alt_tag ) ) {
+				$alt = 'alt="' . $alt_tag . '"';
+			}
 
-		if ( $image_attributes ) {
-			$site_logo = '<img ' . esc_attr( $alt ) . ' src="' . esc_attr( $image_attributes[0] ) . '" width="' . esc_attr( $image_attributes[1] ) . '" height="' . esc_attr( $image_attributes[2] ) . '" />';
-		} ?>
-		<?php do_action( 'boldgrid_site_identity' ); ?>
+			if ( $image_attributes ) {
+				$site_logo = '<img ' . esc_attr( $alt ) . ' src="' . esc_attr( $image_attributes[0] ) . '" width="' . esc_attr( $image_attributes[1] ) . '" height="' . esc_attr( $image_attributes[2] ) . '" />';
+			} ?>
+			<?php do_action( 'boldgrid_site_identity' ); ?>
 
-		<?php if ( has_nav_menu( 'main' ) ) : ?>
-			<!-- Mobile toggle -->
-			<input id="main-menu-state" type="checkbox" />
-			<label class="main-menu-btn" for="main-menu-state">
-				<span class="main-menu-btn-icon"></span><span class="sr-only">Toggle main menu visibility</span>
-			</label>
-			<?php do_action( 'boldgrid_menu_main' ) ?>
-		<?php endif; ?>
+			<?php if ( has_nav_menu( 'main' ) ) : ?>
+				<!-- Mobile toggle -->
+				<input id="main-menu-state" type="checkbox" />
+				<label class="main-menu-btn" for="main-menu-state">
+					<span class="main-menu-btn-icon"></span><span class="sr-only">Toggle main menu visibility</span>
+				</label>
+				<?php do_action( 'boldgrid_menu_main' ) ?>
+			<?php endif; ?>
+		</div>
 	</div>
 	<?php do_action( 'boldgrid_header_bottom' ); ?>
 </header><!-- #masthead -->
