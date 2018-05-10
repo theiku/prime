d229f92e8954ab27cd9d8f446fc28fd221e4d435
BradM <bradm@inmotionhosting.com>
2018-05-10 14:27:51 -0400

    BGTHEME-111 Rework

--- templates/entry-header.php
+++ templates/entry-header.php
@@ -7,17 +7,4 @@
  * @package Prime
  */
 
-// @todo TMP CODE.
-if( ! class_exists( 'BoldGrid_Framework_Title' ) ) {
-	include_once( ABSPATH . BGTFW_PATH . '/includes/class-boldgrid-framework-title.php' );
-}
-
-?>
-
-<?php if ( is_page() || is_single() ) :
-	if ( BoldGrid_Framework_Title::to_show() ) : ?>
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-	<?php endif; ?>
-<?php else : ?>
-	<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-<?php endif; ?>
+the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' );
