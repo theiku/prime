6cc990c012c17e279497a770c8a32b2ef11b7948
Tim <dev@tim.ph>
2018-05-21 23:53:02 -0400

    Update wc templates for 3.4.0

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -10,15 +10,13 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see 	    https://docs.woothemes.com/document/template-structure/
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     3.3.0
+ * @see https://docs.woothemes.com/document/template-structure/
+ * @author WooThemes
+ * @package WooCommerce/Templates
+ * @version 3.4.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+defined( 'ABSPATH' ) || exit;
 
 /**
  * The woocommerce_before_main_content hook.
@@ -28,35 +26,34 @@ if ( ! defined( 'ABSPATH' ) ) {
 	 * @hooked WC_Structured_Data::generate_website_data() - 30
 	*/
 do_action( 'woocommerce_before_main_content' );
-?>
 
+?>
 <header class="woocommerce-products-header">
 	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
 		<h1 class="page-title"><?php woocommerce_page_title(); ?></h1>
 	<?php endif; ?>
 
 	<?php
-		/**
-		 * The woocommerce_archive_description hook.
-		 *
-		 * @hooked woocommerce_taxonomy_archive_description - 10
-		 * @hooked woocommerce_product_archive_description - 10
-		 */
-		do_action( 'woocommerce_archive_description' );
+	/**
+	 * The woocommerce_archive_description hook.
+	 *
+	 * @hooked woocommerce_taxonomy_archive_description - 10
+	 * @hooked woocommerce_product_archive_description - 10
+	 */
+	do_action( 'woocommerce_archive_description' );
 	?>
 </header>
 <?php
-
-if ( have_posts() ) {
+if ( woocommerce_product_loop() ) {
 
 	/**
-	 * The woocommerce_before_shop_loop hook.
+	 * Hook: woocommerce_before_shop_loop.
+	 *
 	 * @hooked wc_print_notices - 10
 	 * @hooked woocommerce_result_count - 20
 	 * @hooked woocommerce_catalog_ordering - 30
 	 */
 	do_action( 'woocommerce_before_shop_loop' );
-
 
 	woocommerce_product_loop_start();
 
@@ -76,10 +73,11 @@ if ( have_posts() ) {
 	}
 
 	woocommerce_product_loop_end();
+
 	/**
 	 * Hook: woocommerce_after_shop_loop.
 	 *
-	 * @hooked woocommerce_pagination - 10s
+	 * @hooked woocommerce_pagination - 10
 	 */
 	do_action( 'woocommerce_after_shop_loop' );
 } else {
@@ -97,4 +95,3 @@ if ( have_posts() ) {
  * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
  */
 do_action( 'woocommerce_after_main_content' );
-?>
--- woocommerce/global/quantity-input.php
+++ woocommerce/global/quantity-input.php
@@ -10,15 +10,12 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see 	    https://docs.woocommerce.com/document/template-structure/
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     3.3.0
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce/Templates
+ * @version 3.4.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+defined( 'ABSPATH' ) || exit;
 
 if ( $max_value && $min_value === $max_value ) {
 	?>
@@ -27,6 +24,8 @@ if ( $max_value && $min_value === $max_value ) {
 	</div>
 	<?php
 } else {
+	/* translators: %s: Quantity. */
+	$labelledby = ! empty( $args['product_name'] ) ? sprintf( __( '%s quantity', 'woocommerce' ), strip_tags( $args['product_name'] ) ) : '';
 	?>
 	<div class="input-group quantity">
 		<span class="input-group-btn">
@@ -34,7 +33,20 @@ if ( $max_value && $min_value === $max_value ) {
 				<span class="fa fa-minus"></span>
 			</button>
 		</span>
-		<input type="number" id="<?php echo esc_attr( $input_id ); ?>" class="form-control input-number input-text qty text" step="<?php echo esc_attr( $step ); ?>" min="<?php echo esc_attr( $min_value ); ?>" max="<?php echo esc_attr( 0 < $max_value ? $max_value : '' ); ?>" name="<?php echo esc_attr( $input_name ); ?>" value="<?php echo esc_attr( $input_value ); ?>" title="<?php echo esc_attr_x( 'Qty', 'Product quantity input tooltip', 'woocommerce' ) ?>" size="4" pattern="<?php echo esc_attr( $pattern ); ?>" inputmode="<?php echo esc_attr( $inputmode ); ?>" aria-labelledby="<?php echo ! empty( $args['product_name'] ) ? sprintf( esc_attr__( '%s quantity', 'woocommerce' ), $args['product_name'] ) : ''; ?>" />
+		<input
+			type="number"
+			id="<?php echo esc_attr( $input_id ); ?>"
+			class="form-control input-number input-text qty text"
+			step="<?php echo esc_attr( $step ); ?>"
+			min="<?php echo esc_attr( $min_value ); ?>"
+			max="<?php echo esc_attr( 0 < $max_value ? $max_value : '' ); ?>"
+			name="<?php echo esc_attr( $input_name ); ?>"
+			value="<?php echo esc_attr( $input_value ); ?>"
+			title="<?php echo esc_attr_x( 'Qty', 'Product quantity input tooltip', 'woocommerce' ) ?>"
+			size="4"
+			pattern="<?php echo esc_attr( $pattern ); ?>"
+			inputmode="<?php echo esc_attr( $inputmode ); ?>"
+			aria-labelledby="<?php echo esc_attr( $labelledby ); ?>" />
 		<span class="input-group-btn">
 			<button type="button" class="btn btn-plus btn-number" data-type="plus" data-field="<?php echo esc_attr( $input_name ); ?>">
 				<span class="fa fa-plus"></span>
