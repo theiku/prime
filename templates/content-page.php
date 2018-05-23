6ddf78bd0672bbe24de02eed6cdf34e1d4af7da0
Tim <dev@tim.ph>
2018-05-22 20:20:14 -0400

    removing ctas

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,24 +1,3 @@
-<?php
-/**
- * Default Page Template.
- *
- * This is the template responsible for displaying the
- * default page in the BoldGrid Theme.
- *
- * @since 2.0
- * @package Prime
- */
-
-// Get the theme configs.
-global $boldgrid_theme_framework;
-$configs = $boldgrid_theme_framework->get_configs();
-
-// Display the call to action widget area if configs are set.
-$cta = $configs['template']['call-to-action'];
-if ( 'all-pages' === $cta ) {
-	include locate_template( 'templates/call-to-action.php' );
-}
-?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php do_action( 'before_entry_title' ); ?>
 	<header class="entry-header">
