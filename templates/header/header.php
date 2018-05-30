53778b5e043ddd883bc8e20d2d5cb7261be0ca27
Tim <dev@tim.ph>
2018-05-29 20:35:13 -0400

    move toggle into dynamic hooks

--- templates/header/header.php
+++ templates/header/header.php
@@ -14,14 +14,10 @@
 		<?php the_custom_header_markup(); ?>
 	</div>
 	<div id="navi-wrap" <?php BoldGrid::add_class( 'navi_wrap' ); ?>>
-		<?php do_action( 'boldgrid_menu_secondary', [ 'container_class' => BoldGrid::get_container_classes( 'header' ) ] ); ?>
+		<?php do_action( 'boldgrid_menu_secondary', [ 'menu_class' => BoldGrid::get_container_classes( 'header' ) ] ); ?>
 		<div id="navi" <?php BoldGrid::add_class( 'navi' ); ?>>
 			<?php do_action( 'boldgrid_site_identity' ); ?>
 			<?php if ( has_nav_menu( 'main' ) ) : ?>
-				<input id="main-menu-state" type="checkbox" />
-				<label class="main-menu-btn" for="main-menu-state">
-					<span class="main-menu-btn-icon"></span><span class="screen-reader-text">Toggle main menu visibility</span>
-				</label>
 				<?php do_action( 'boldgrid_menu_main' ) ?>
 			<?php endif; ?>
 		</div>
