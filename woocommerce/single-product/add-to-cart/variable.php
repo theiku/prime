5923114d01ad9d4ad8b3ffd30c03aa8c7e80a7b5
Nicole <nicolepaschen42@gmail.com>
2018-06-04 10:26:47 -0400

    updated variable product to 3.4.1'

--- woocommerce/single-product/add-to-cart/variable.php
+++ woocommerce/single-product/add-to-cart/variable.php
@@ -10,9 +10,9 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see 	https://docs.woocommerce.com/document/template-structure/
+ * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates
- * @version 3.4.0
+ * @version 3.4.1
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -33,34 +33,32 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 			<tbody>
 				<?php foreach ( $attributes as $attribute_name => $options ) : ?>
 					<tr>
-						<td class="label color2-color"><label for="<?php echo esc_attr( sanitize_title( $attribute_name ) ); ?>"><?php echo esc_html( wc_attribute_label( $attribute_name ) ); ?></label></td>
+						<td class="label color2-color"><label for="<?php echo esc_attr( sanitize_title( $attribute_name ) ); ?>"><?php echo wc_attribute_label( $attribute_name ); // WPCS: XSS ok. ?></label></td>
 						<td class="value">
 							<?php
-								$selected = isset( $_REQUEST[ 'attribute_' . $attribute_name ] ) ? wc_clean( urldecode( wp_unslash( $_REQUEST[ 'attribute_' . $attribute_name ] ) ) ) : $product->get_variation_default_attribute( $attribute_name ); // WPCS: input var ok, CSRF ok, sanitization ok.
 								wc_dropdown_variation_attribute_options( array(
 									'options' => $options,
 									'attribute' => $attribute_name,
 									'product' => $product,
-									'selected' => $selected
 								) );
-
 								echo end( $attribute_keys ) === $attribute_name ? wp_kses_post( apply_filters( 'woocommerce_reset_variations_link', '<a class="reset_variations" href="#">' . esc_html__( 'Clear', 'woocommerce' ) . '</a>' ) ) : '';
 							?>
 						</td>
 					</tr>
-				<?php endforeach;?>
+				<?php endforeach; ?>
 			</tbody>
 		</table>
 
 		<div class="single_variation_wrap">
 			<?php
 				/**
-				 * woocommerce_before_single_variation Hook.
+				 * Hook: woocommerce_before_single_variation.
 				 */
 				do_action( 'woocommerce_before_single_variation' );
 
 				/**
-				 * woocommerce_single_variation hook. Used to output the cart button and placeholder for variation data.
+				 * Hook: woocommerce_single_variation. Used to output the cart button and placeholder for variation data.
+				 *
 				 * @since 2.4.0
 				 * @hooked woocommerce_single_variation - 10 Empty div for variation data.
 				 * @hooked woocommerce_single_variation_add_to_cart_button - 20 Qty and cart button.
@@ -68,12 +66,11 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 				do_action( 'woocommerce_single_variation' );
 
 				/**
-				 * woocommerce_after_single_variation Hook.
+				 * Hook: woocommerce_after_single_variation.
 				 */
 				do_action( 'woocommerce_after_single_variation' );
 			?>
 		</div>
-
 	<?php endif; ?>
 
 	<?php do_action( 'woocommerce_after_variations_form' ); ?>
