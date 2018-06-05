5960a60e9639e2ea288a2b77679b12b805bd3f92
Tim <dev@tim.ph>
2018-06-05 19:32:21 -0400

    moving secondary menu out of navi wrap

--- templates/header/header.php
+++ templates/header/header.php
@@ -13,8 +13,8 @@
 	<div class="custom-header-media">
 		<?php the_custom_header_markup(); ?>
 	</div>
+	<?php do_action( 'boldgrid_menu_secondary', [ 'menu_class' => BoldGrid::get_container_classes( 'header' ) ] ); ?>
 	<div id="navi-wrap" <?php BoldGrid::add_class( 'navi_wrap' ); ?>>
-		<?php do_action( 'boldgrid_menu_secondary', [ 'menu_class' => BoldGrid::get_container_classes( 'header' ) ] ); ?>
 		<div id="navi" <?php BoldGrid::add_class( 'navi' ); ?>>
 			<?php do_action( 'boldgrid_site_identity' ); ?>
 			<?php if ( has_nav_menu( 'main' ) ) : ?>
