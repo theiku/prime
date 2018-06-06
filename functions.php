3abbffe8cf88d13f4b26c4931343e77b3b599ceb
bradm@inmotionhosting.com <bradm@inmotionhosting.com>
2018-06-06 13:48:11 -0400

    Revert "BGTHEME-299 escaping data"
    This reverts commit e17d0ae4b07404e64645a883acb334ad46a79b2f.

--- functions.php
+++ functions.php
@@ -5,12 +5,5 @@
  * @package Prime
  */
 
-//locate_template( '/inc/boldgrid-theme-framework-config/config.php', true, true );
-//require_once get_template_directory() . '/inc/boldgrid-theme-framework/boldgrid-theme-framework.php';
-
-$catfish = '/home/bradm/public_html/php-54/single-site-1/wp-content/themes';
-//require_once $catfish . '/boldgrid-theme-framework//boldgrid-theme-framework/includes/class-boldgrid-framework-system-requirements.php';
-//$bgtfw_requirements = new Boldgrid_Framework_System_Requirements();
-//$bgtfw_requirements->add_hooks();
 locate_template( '/inc/boldgrid-theme-framework-config/config.php', true, true );
-require_once $catfish . '/boldgrid-theme-framework//boldgrid-theme-framework/boldgrid-theme-framework.php';
+require_once get_template_directory() . '/inc/boldgrid-theme-framework/boldgrid-theme-framework.php';
