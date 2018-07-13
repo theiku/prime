b74d204903104de8d02f562469f05e5f19bf07f1
Tim <dev@tim.ph>
2018-07-13 13:56:45 -0400

    remove h1s

--- templates/entry-header.php
+++ templates/entry-header.php
@@ -1,6 +1,10 @@
 <header class="entry-header" <?php is_single() ? : bgtfw_featured_img_bg( $post->ID ); ?>>
 	<div class="featured-imgage-header">
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+		<?php if ( is_single() || is_page() ) : ?>
+			<?php the_title( '<p class="h1 page-title">', '</p>' ); ?>
+		<?php else : ?>
+			<?php the_title( sprintf( '<p class="h1 page-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></p>' ); ?>
+		<?php endif; ?>
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php boldgrid_posted_on(); ?>
