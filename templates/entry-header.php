b6b9ae5e409d2302831c01646ae4afa0d9bf3093
Tim <dev@tim.ph>
2018-07-13 13:59:05 -0400

    entry title

--- templates/entry-header.php
+++ templates/entry-header.php
@@ -1,9 +1,9 @@
 <header class="entry-header" <?php is_single() ? : bgtfw_featured_img_bg( $post->ID ); ?>>
 	<div class="featured-imgage-header">
 		<?php if ( is_single() || is_page() ) : ?>
-			<?php the_title( '<p class="h1 page-title">', '</p>' ); ?>
+			<?php the_title( '<p class="h1 entry-title page-title">', '</p>' ); ?>
 		<?php else : ?>
-			<?php the_title( sprintf( '<p class="h1 page-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></p>' ); ?>
+			<?php the_title( sprintf( '<p class="h1 entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></p>' ); ?>
 		<?php endif; ?>
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
