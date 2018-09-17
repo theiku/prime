5e869fefe56e86985018b5da1ad0a395803c5084
Rafael Ramos <rramo012@gmail.com>
2018-09-17 17:25:01 -0400

    Setting up widget components and posts

---
+++ starter-content/corporate/blog/generic.php
@@ -0,0 +1,17 @@
+<div class="boldgrid-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-xs-12 col-sm-12">
+				<h3>Plan Your Content</h3>
+				<p class="">If you're considering adding a blog to your site, you'll want to have a plan beforehand. Planning your blog will help your subject matter remain consistent over time. It'll also help you determine whether or not there's enough material to maintain a steady stream of posts.</p>
+				<p class="">One pitfall many new bloggers run into is starting a blog that isn't posted to frequently enough. A shortage of recent posts can give your visitors a bad impression of your business. One may think "I wonder if they’re still in business" or "they may want to hire a writer."</p>
+				<p class="">A blog, like any other customer facing aspect of your business, communicates your brand. If it isn't maintained and given proper attention, people will notice. Post regularly and keep your content fresh. Give your audience a reason to visit often.</p>
+				<p class="">
+				<!--more Read more &gt; -->
+				</p>
+				<h3>Find Your Audience</h3>
+				<p class="">While on the topic of audiences, you'll likely want to identify yours early on. If your blog is going to be set up to compliment a business, your target audience will likely be the same as your consumer base; you're then writing for the same people that buy your product. You'll want to allow any marketing material you've used inform the style and tone of your writing. Think of your blog as an extension of your company's brand. If, on the other hand, your business is completely new or you don't happen to be selling anything in particular, this is the time to start thinking about your brand.</p>
+			</div>
+		</div>
+	</div>
+</div>
