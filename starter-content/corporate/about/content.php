4b8aa8f1df08fa33c978b1d5ccad7e672e3cf2e6
Nicole <nicolec@boldgrid.com>
2018-09-18 15:37:04 -0400

    changed px to em

--- starter-content/corporate/about/content.php
+++ starter-content/corporate/about/content.php
@@ -6,13 +6,13 @@
 			</div>
 			<div class="col-md-1 col-sm-1 col-xs-12"></div>
 			<div class="col-md-6 col-sm-6 col-xs-12">
-				<p class="color1-color h6" style="margin-bottom: 5px; text-transform: uppercase;">Your Best Solution</p>
+				<p class="color1-color h6" style="margin-bottom: .3em; text-transform: uppercase;">Your Best Solution</p>
 				<h2 style="margin-top: 0;">What We Do</h2>
 				<?php $divider(); ?>
-				<p style="margin-top: 35px;">Leveraging sprints with the aim to take this offline. Executing transformation mapping with a goal to build ROI. Taking dark social to in turn think outside the box. Execute cloud computing to, consequently, go viral. Utilise below the
+				<p style="margin-top: 2.2em;">Leveraging sprints with the aim to take this offline. Executing transformation mapping with a goal to build ROI. Taking dark social to in turn think outside the box. Execute cloud computing to, consequently, go viral. Utilise below the
 					fold and then re-target key demographics.</p>
-				<p style="margin-top: 35px;">Inform user experience to, consequently, funnel users. Amplifying vertical integration yet be CMSable. Demonstrating customer journeys but target the low hanging fruit.</p>
-				<p style="margin-top: 35px;"><a class="button-primary" href="#">Learn More</a></p>
+				<p style="margin-top: 2.2em;">Inform user experience to, consequently, funnel users. Amplifying vertical integration yet be CMSable. Demonstrating customer journeys but target the low hanging fruit.</p>
+				<p style="margin-top: 2.2em;"><a class="button-primary" href="#">Learn More</a></p>
 			</div>
 		</div>
 	</div>
@@ -49,20 +49,20 @@
 	<div class="container">
 		<div class="row" style="padding-bottom: 100px; padding-top: 100px;">
 			<div class="col-md-6 col-sm-6 col-xs-12">
-				<p class="color1-color h6" style="margin-bottom: 5px; text-transform: uppercase;">Spark Better Business</p>
-				<h2 style="margin-top: 0px;">Why Choose Us</h2>
+				<p class="color1-color h6" style="margin-bottom: .3em; text-transform: uppercase;">Spark Better Business</p>
+				<h2 style="margin-top: 0;">Why Choose Us</h2>
 				<?php $divider(); ?>
-				<p style="margin-top: 35px;">Leveraging sprints with the aim to take this offline. Executing transformation mapping with a goal to build ROI. Taking dark social to in turn think outside the box.</p>
-				<p style="margin-top: 35px;">Execute cloud computing to, consequently, go viral. Utilise below the fold and then re-target key demographics.</p>
-				<div class="row" style="margin-top: 35px;">
+				<p style="margin-top: 2.2em;">Leveraging sprints with the aim to take this offline. Executing transformation mapping with a goal to build ROI. Taking dark social to in turn think outside the box.</p>
+				<p style="margin-top: 2.2em;">Execute cloud computing to, consequently, go viral. Utilise below the fold and then re-target key demographics.</p>
+				<div class="row" style="margin-top: 2.2em;">
 					<div class="col-md-6 col-xs-12 col-sm-12">
-						<h4 class="h5"><i class="fa bg-inserted-icon fa-check-square-o color1-color" style="margin: 0px 15px 0px 0px; font-size: 20px;" aria-hidden="true"><span style="display: none;">&nbsp;</span></i>Transformation</h4>
-						<h4 class="h5" style="margin-top: 35px;"><i class="fa bg-inserted-icon fa-check-square-o color1-color" style="margin: 0px 15px 0px 0px; font-size: 20px;" aria-hidden="true"><span style="display: none;">&nbsp;</span></i>Operations &amp; Robotics</h4>
+						<h4 class="h5"><i class="fa bg-inserted-icon fa-check-square-o color1-color" style="margin: 0em 0.8em 0em 0em; font-size: 20px;" aria-hidden="true"><span style="display: none;">&nbsp;</span></i>Transformation</h4>
+						<h4 class="h5" style="margin-top: 2.2em;"><i class="fa bg-inserted-icon fa-check-square-o color1-color" style="margin: 0em 0.8em 0em 0em; font-size: 20px;" aria-hidden="true"><span style="display: none;">&nbsp;</span></i>Operations &amp; Robotics</h4>
 					</div>
 					<div class="col-md-6 col-xs-12 col-sm-12">
 						<div>
-							<h4 class="h5"><i class="fa bg-inserted-icon fa-check-square-o color1-color" style="margin: 0px 15px 0px 0px; font-size: 20px;" aria-hidden="true"> </i>Information Technology</h4>
-							<h4 class="h5" style="margin-top: 35px;"><i class="fa bg-inserted-icon fa-check-square-o color1-color" style="margin: 0px 15px 0px 0px; font-size: 20px;" aria-hidden="true"> </i>Unlimited Possibilites</h4>
+							<h4 class="h5"><i class="fa bg-inserted-icon fa-check-square-o color1-color" style="margin: 0em 0.8em 0em 0em; font-size: 20px;" aria-hidden="true"> </i>Information Technology</h4>
+							<h4 class="h5" style="margin-top: 2.2em;"><i class="fa bg-inserted-icon fa-check-square-o color1-color" style="margin: 0em 0.8em 0em 0em; font-size: 20px;" aria-hidden="true"> </i>Unlimited Possibilites</h4>
 						</div>
 					</div>
 				</div>
@@ -83,7 +83,7 @@
 				</div>
 				<div class="col-md-8 col-sm-7 col-xs-12">
 					<p class="h4">Leveraging sprints with the aim to take this offline. Executing transformation mapping with a goal to build ROI. Taking dark social to in turn think outside the box.</p>
-					<p class="h4" style="text-align: right; margin-top: 75px;">Starr Hudson</p>
+					<p class="h4" style="text-align: right; margin-top: 4.6em;">Starr Hudson</p>
 					<p style="text-align: right;" class="color1-color">Chief Executive Officer | Box Em Up</p>
 				</div>
 			</div>
@@ -97,7 +97,7 @@
 				</div>
 				<div class="col-md-8 col-sm-7 col-xs-12">
 					<p class="h4">Engaging customer experience so that as an end result, we be CMSable. Leverage below the fold and finally gain traction. Generating bleeding edge and creating actionable insights.</p>
-					<p class="h4" style="text-align: right; margin-top: 75px;">Teresa Oliver</p>
+					<p class="h4" style="text-align: right; margin-top: 4.6em;">Teresa Oliver</p>
 					<p style="text-align: right;" class="color1-color">Brand Expert | Mantell Design</p>
 				</div>
 			</div>
@@ -111,7 +111,7 @@
 				</div>
 				<div class="col-md-8 col-sm-7 col-xs-12">
 					<p class="h4"> Generating best in class in order to improve overall outcomes. Inform outside the box thinking and finally target the low hanging fruit.</p>
-					<p class="h4" style="text-align: right; margin-top: 75px;">Leslie Boone</p>
+					<p class="h4" style="text-align: right; margin-top: 4.6em;">Leslie Boone</p>
 					<p style="text-align: right;" class="color1-color">Support Staff | Gerwyn Financial</p>
 				</div>
 			</div>
@@ -122,39 +122,39 @@
 	<div class="container">
 		<div class="row" style="padding-top: 100px; padding-bottom: 35px;">
 			<div class="col-md-4 col-sm-4 col-xs-12">
-				<p class="color1-color h6" style="margin-bottom: 5px; text-transform: uppercase;">Meet the Professionals</p>
-				<h2 style="margin-top: 0px;">Team</h2>
+				<p class="color1-color h6" style="margin-bottom: .3em; text-transform: uppercase;">Meet the Professionals</p>
+				<h2 style="margin-top: 0;">Team</h2>
 				<?php $divider(); ?>
 			</div>
 			<div class="col-md-1 col-sm-1 col-xs-12"></div>
 			<div class="col-md-7 col-xs-12 col-sm-7">
-				<p style="margin-top: 35px;">Target user stories so that we maximise share of voice. Grow cloud computing with the aim to take this offline. Lead integrated tech stacks and above all, target the low hanging fruit.</p>
+				<p style="margin-top: 2.2em;">Target user stories so that we maximise share of voice. Grow cloud computing with the aim to take this offline. Lead integrated tech stacks and above all, target the low hanging fruit.</p>
 			</div>
 		</div>
 		<div class="row" style="padding-bottom: 100px;">
 			<div class="col-md-3 col-xs-12 col-sm-6">
-				<div class="text-center bg-box bg-box-square bg-box-cover color-3-text-contrast color3-background-color" style="padding: 0.5em 0.5em 1em; margin: 0px -13px;">
+				<div class="text-center bg-box bg-box-square bg-box-cover color-3-text-contrast color3-background-color" style="padding: 0.5em 0.5em 1em; margin: 0em -.8em;">
 					<p><img src="<?php $image_path('about/about5.png') ?>"></p>
 					<h4>Nathan Counsel</h4>
 					<p class="color1-color">Chief Executive Officer</p>
 				</div>
 			</div>
 			<div class="col-md-3 col-xs-12 col-sm-6">
-				<div class="text-center bg-box bg-box-square bg-box-cover color-3-text-contrast color3-background-color" style="padding: 0.5em 0.5em 1em; margin: 0px -13px;">
+				<div class="text-center bg-box bg-box-square bg-box-cover color-3-text-contrast color3-background-color" style="padding: 0.5em 0.5em 1em; margin: 0em -.8em;">
 					<p><img src="<?php $image_path('about/about6.png') ?>"></p>
 					<h4>Morten Iverson</h4>
 					<p class="color1-color">Chief Financial Officer</p>
 				</div>
 			</div>
 			<div class="col-md-3 col-xs-12 col-sm-6">
-				<div class="text-center bg-box bg-box-square bg-box-cover color-3-text-contrast color3-background-color" style="padding: 0.5em 0.5em 1em; margin: 0px -13px;">
+				<div class="text-center bg-box bg-box-square bg-box-cover color-3-text-contrast color3-background-color" style="padding: 0.5em 0.5em 1em; margin: 0em -.8em;">
 					<p><img src="<?php $image_path('about/about7.png') ?>"></p>
 					<h4>Julie Grave</h4>
 					<p class="color1-color">Information Technology Officer</p>
 				</div>
 			</div>
 			<div class="col-md-3 col-xs-12 col-sm-6">
-				<div class="text-center bg-box bg-box-square bg-box-cover color-3-text-contrast color3-background-color" style="padding: 0.5em 0.5em 1em; margin: 0px -13px;">
+				<div class="text-center bg-box bg-box-square bg-box-cover color-3-text-contrast color3-background-color" style="padding: 0.5em 0.5em 1em; margin: 0em -.8em;">
 					<p><img src="<?php $image_path('about/about8.png') ?>"></p>
 					<h4>Hans Paulsen</h4>
 					<p class="color1-color">Operations Director</p>
--- starter-content/corporate/services/content.php
+++ starter-content/corporate/services/content.php
@@ -33,22 +33,22 @@
 		<div class="row" style="padding-bottom: 80px;">
 			<div class="col-md-3 col-xs-12 col-sm-6 text-center" style="padding: 2em;">
 				<p><img src="<?php $image_path( 'icons/008-idea.png' ) ?>" alt="" width="75" height="75"></p>
-				<h3 style="margin-top: 35px;">Discover</h3>
+				<h3 style="margin-top: 2.1em;">Discover</h3>
 				<p class="">Lead bleeding edge and then further your reach. Synchronizing below the line.</p>
 			</div>
 			<div class="col-md-3 col-xs-12 col-sm-6 text-center" style="padding: 2em;">
 				<p><img src="<?php $image_path( 'icons/009-options.png' ) ?>" alt="" width="75" height="75"></p>
-				<h3 style="margin-top: 35px;">Create</h3>
+				<h3 style="margin-top: 2.1em;">Create</h3>
 				<p class="">Lead bleeding edge and then further your reach. Synchronizing below the line.</p>
 			</div>
 			<div class="col-md-3 col-xs-12 col-sm-6 text-center" style="padding: 2em;">
 				<p><img src="<?php $image_path( 'icons/010-start.png' ) ?>" alt="" width="62" height="75"></p>
-				<h3 style="margin-top: 35px;">Energy</h3>
+				<h3 style="margin-top: 2.1em;">Energy</h3>
 				<p class="">Lead bleeding edge and then further your reach. Synchronizing below the line.</p>
 			</div>
 			<div class="col-md-3 col-xs-12 col-sm-6 text-center" style="padding: 2em;">
 				<p><img src="<?php $image_path( 'icons/011-develop.png' ) ?>" alt="" width="75" height="75"></p>
-				<h3 style="margin-top: 35px;">Success</h3>
+				<h3 style="margin-top: 2.1em;">Success</h3>
 				<p class="">Lead bleeding edge and then further your reach. Synchronizing below the line.</p>
 			</div>
 		</div>
@@ -58,8 +58,8 @@
 	<div class="container">
 		<div class="row" style="padding-top: 60px; padding-bottom: 60px;">
 			<div class="col-md-5 col-sm-4 col-xs-12">
-				<p class="color1-color h6" style="margin-bottom: 5px; text-transform: uppercase;">Our Best Business</p>
-				<h2 style="margin-top: 0px;">The Benefit</h2>
+				<p class="color1-color h6" style="margin-bottom: .3em; text-transform: uppercase;">Our Best Business</p>
+				<h2 style="margin-top: 0;">The Benefit</h2>
 				<?php $divider(); ?>
 			</div>
 			<div class="col-md-7 col-xs-12 col-sm-7"></div>
@@ -78,13 +78,13 @@
 	<div class="container">
 		<div class="row" style="padding-top: 80px; padding-bottom: 80px;">
 			<div class="col-md-5 col-sm-4 col-xs-12">
-				<p style="margin-bottom: 5px; text-transform: uppercase;">Take a Look</p>
-				<h2 style="color: #fff; margin-top: 0px;">Glimpse</h2>
+				<p style="margin-bottom: .3em; text-transform: uppercase;">Take a Look</p>
+				<h2 style="color: #fff; margin-top: 0;">Glimpse</h2>
 				<?php $divider(); ?>
 			</div>
 			<div class="col-md-7 col-xs-12 col-sm-12">
-				<p style="margin-top: 35px;">Executing audience segments and try to be transparent. Generating analytics with a goal to build ROI. Grow stakeholder management so that we surprise and delight.</p>
-				<div class="row bg-editor-hr-wrap" style="margin-top: 35px;">
+				<p style="margin-top: 2.1em;">Executing audience segments and try to be transparent. Generating analytics with a goal to build ROI. Grow stakeholder management so that we surprise and delight.</p>
+				<div class="row bg-editor-hr-wrap" style="margin-top: 2.1em;">
 					<div class="col-md-12 col-xs-12 col-sm-12">
 						<div>
 							<hr>
@@ -176,10 +176,10 @@
 </div>
 <div class="boldgrid-section">
 	<div class="container">
-		<div class="row" style="padding-top: 60px; padding-bottom: 0px;">
+		<div class="row" style="padding-top: 60px;">
 			<div class="col-md-5 col-sm-4 col-xs-12">
-				<p class="color1-color h6" style="text-transform: uppercase; margin-bottom: 5px;">Pick Your Plan</p>
-				<h2 style="margin-top: 0px;">Pricing Table</h2>
+				<p class="color1-color h6" style="text-transform: uppercase; margin-bottom: .3em;">Pick Your Plan</p>
+				<h2 style="margin-top: 0;">Pricing Table</h2>
 				<?php $divider(); ?>
 			</div>
 			<div class="col-md-7 col-xs-12 col-sm-7"></div>
@@ -188,11 +188,11 @@
 </div>
 <div class="tmpl-pricing_tables-9 boldgrid-section dynamic-gridblock">
 	<div class="container">
-		<div class="row" style="padding-top: 25px;padding-bottom: 100px;">
+		<div class="row" style="padding-top: 25px; padding-bottom: 100px;">
 			<div class="col-md-3 col-xs-12 col-sm-3 text-center align-column-center">
-				<div class="bg-box-flush bg-box-cover basic product-type color-neutral-background-color color-neutral-text-contrast bg-background-color bg-box" style="margin-top: 10px; margin-bottom: 10px;">
+				<div class="bg-box-flush bg-box-cover basic product-type color-neutral-background-color color-neutral-text-contrast bg-background-color bg-box" style="margin-top: .6em; margin-bottom: .6em;">
 					<p class="product-name">STANDARD</p>
-					<p class="price" style="background-color: rgba(0,0,0,.05);margin-top: 0;"> <span style="font-size: 1.5em; position: relative; top:-15px;" class="color2-color">$</span> <span style="font-size: 2.5em;" class="color2-color">39</span> <span style="margin-top: 0; font-size: .9em; font-style: italic; display: block;">Yearly</span>                        </p>
+					<p class="price" style="background-color: rgba(0,0,0,.05);margin-top: 0;"> <span style="font-size: 1.5em; position: relative; top: -.9em;" class="color2-color">$</span> <span style="font-size: 2.5em;" class="color2-color">39</span> <span style="margin-top: 0; font-size: .9em; font-style: italic; display: block;">Yearly</span>                        </p>
 					<ul class="feature-list bg-list">
 						<li>Feature 1</li>
 						<li>Feature 2</li>
@@ -210,9 +210,9 @@
 
 			</div>
 			<div class="col-md-3 col-xs-12 col-sm-3 text-center align-column-center">
-				<div class="bg-box-flush bg-box-cover basic product-type color-neutral-background-color color-neutral-text-contrast bg-background-color bg-box" style="margin-top: 10px; margin-bottom: 10px;">
+				<div class="bg-box-flush bg-box-cover basic product-type color-neutral-background-color color-neutral-text-contrast bg-background-color bg-box" style="margin-top: .6em; margin-bottom: .6em;">
 					<p class="product-name">BUSINESS</p>
-					<p class="price" style="background-color: rgba(0,0,0,.05);margin-top: 0;"> <span style="font-size: 1.5em; position: relative; top:-15px;" class="color2-color">$</span> <span style="font-size: 2.5em;" class="color2-color">59</span> <span style="margin-top: 0; font-size: .9em; font-style: italic; display: block;">Yearly</span>                        </p>
+					<p class="price" style="background-color: rgba(0,0,0,.05);margin-top: 0;"> <span style="font-size: 1.5em; position: relative; top: -.9em;" class="color2-color">$</span> <span style="font-size: 2.5em;" class="color2-color">59</span> <span style="margin-top: 0; font-size: .9em; font-style: italic; display: block;">Yearly</span>                        </p>
 					<ul class="feature-list bg-list">
 						<li>Feature 1</li>
 						<li>Feature 2</li>
@@ -233,7 +233,7 @@
 			<div class="col-md-3 col-xs-12 col-sm-3 text-center align-column-center bg-box-border-thin color3-border-color" style="position:relative; z-index:1">
 				<div class="bg-box-flush bg-box-cover featured product-type color-neutral-background-color color-neutral-text-contrast bg-background-color bg-box">
 					<p class="product-name">PREMIUM</p>
-					<p class="price" style="background-color: rgba(0,0,0,.05);margin-top: 0;"> <span style="font-size: 1.5em; position: relative; top:-15px;" class="color2-color">$</span> <span style="font-size: 2.5em;" class="color2-color">79</span> <span style="margin-top: 0; font-size: .9em; font-style: italic; display: block;">Yearly</span>                        </p>
+					<p class="price" style="background-color: rgba(0,0,0,.05);margin-top: 0;"> <span style="font-size: 1.5em; position: relative; top: -.9em;" class="color2-color">$</span> <span style="font-size: 2.5em;" class="color2-color">79</span> <span style="margin-top: 0; font-size: .9em; font-style: italic; display: block;">Yearly</span>                        </p>
 					<ul class="feature-list bg-list">
 						<li>Feature 1</li>
 						<li>Feature 2</li>
@@ -252,9 +252,9 @@
 
 			</div>
 			<div class="col-md-3 col-xs-12 col-sm-3 text-center align-column-center">
-				<div class="bg-box-flush bg-box-cover basic product-type color-neutral-background-color color-neutral-text-contrast bg-background-color bg-box" style="margin-top: 10px; margin-bottom: 10px;">
+				<div class="bg-box-flush bg-box-cover basic product-type color-neutral-background-color color-neutral-text-contrast bg-background-color bg-box" style="margin-top: .6em; margin-bottom: .6em;">
 					<p class="product-name">ULTIMATE</p>
-					<p class="price" style="background-color: rgba(0,0,0,.05);margin-top: 0;"> <span style="font-size: 1.5em; position: relative; top:-15px;" class="color2-color">$</span> <span style="font-size: 2.5em;" class="color2-color">109</span> <span style="margin-top: 0; font-size: .9em; font-style: italic; display: block;">Yearly</span>                        </p>
+					<p class="price" style="background-color: rgba(0,0,0,.05);margin-top: 0;"> <span style="font-size: 1.5em; position: relative; top: -.9em;" class="color2-color">$</span> <span style="font-size: 2.5em;" class="color2-color">109</span> <span style="margin-top: 0; font-size: .9em; font-style: italic; display: block;">Yearly</span>                        </p>
 					<ul class="feature-list bg-list">
 						<li>Feature 1</li>
 						<li>Feature 2</li>
