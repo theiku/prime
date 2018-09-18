3515d52edddf8f150318327aa43c7a2d4d894adf
Nicole <nicolec@boldgrid.com>
2018-09-18 10:02:43 -0400

    updated top block of services page

--- starter-content/corporate/services/content.php
+++ starter-content/corporate/services/content.php
@@ -12,47 +12,44 @@
 		'widget-boldgrid_component_postlist[][image_size]' => 'medium',
 	] ) );
 } ?>
-<div class="boldgrid-section" style="background-image: linear-gradient(to left, rgba(33, 33, 33, 0.5), rgba(33, 33, 33, 0.5)), url('<?php $image_path( 'services/services1.png' ) ?>'); background-size: cover; background-position: 50% 50%;" data-bg-overlaycolor="rgba(33,33,33,0.5)">
+<div class="boldgrid-section">
 	<div class="container">
-		<div class="row" style="padding-top: 45px; padding-bottom: 35px;">
-			<div class="col-md-2 col-sm-12 col-xs-12"></div>
-			<div class="col-md-8 col-xs-12 col-sm-12 text-center">
-				<p style="color: #fff; text-transform: uppercase; margin-bottom: 0px;">Take Your Steps</p>
-				<h2 style="color: #fff; margin-top: 0px;">Winning Process</h2>
-				<div class="row bg-editor-hr-wrap">
-					<div class="col-md-12 col-xs-12 col-sm-12">
-						<div>
-							<hr class="bg-hr bg-hr-15 color1-color" style="width: 10%; margin-left: auto; margin-right: auto; margin-top: 0px;">
-						</div>
-					</div>
-				</div>
+		<div class="row" style="padding-top: 80px; padding-bottom: 60px;">
+			<div class="col-md-6 col-sm-12 col-xs-12">
+				<p class="h6 color1-color" style="margin-bottom: 0; text-transform: uppercase;">Take Your Steps</p>
+				<h2 class="" style="margin-top: 0;">Winning Process</h2>
+				<?php $divider(); ?>
+				<p class="" style="margin-bottom: 1.5em;">Leverage integrated tech stacks and above all, build ROI. Targeting user engagement and try to further your reach. Building awareness while remembering to target the low hanging fruit. Growing brand ambassadors with a goal to maximise share of voice.</p>
+				<a class="button-primary" href="#">Learn More</a>
 			</div>
-			<div class="col-md-2 col-sm-12 col-xs-12"></div>
+			<div class="col-md-6 col-sm-12 col-xs-12 align-column-center">
+				<p><img class="image-shadow" src="<?php $image_path('services/services1.png') ?>"></p>
+			</div>
 		</div>
 	</div>
 </div>
 <div class="boldgrid-section">
 	<div class="container">
-		<div class="row" style="padding-top: 80px; padding-bottom: 80px;">
-			<div class="col-md-3 col-xs-12 col-sm-3 text-center" style="padding: 2em;">
+		<div class="row" style="padding-bottom: 80px;">
+			<div class="col-md-3 col-xs-12 col-sm-6 text-center" style="padding: 2em;">
 				<p><img src="<?php $image_path( 'icons/008-idea.png' ) ?>" alt="" width="75" height="75"></p>
 				<h3 style="margin-top: 35px;">Discover</h3>
-				<p>Lead bleeding edge and then further your reach. Synchronizing below the line.</p>
+				<p class="">Lead bleeding edge and then further your reach. Synchronizing below the line.</p>
 			</div>
-			<div class="col-md-3 col-xs-12 col-sm-3 text-center" style="padding: 2em;">
+			<div class="col-md-3 col-xs-12 col-sm-6 text-center" style="padding: 2em;">
 				<p><img src="<?php $image_path( 'icons/009-options.png' ) ?>" alt="" width="75" height="75"></p>
 				<h3 style="margin-top: 35px;">Create</h3>
-				<p>Lead bleeding edge and then further your reach. Synchronizing below the line.</p>
+				<p class="">Lead bleeding edge and then further your reach. Synchronizing below the line.</p>
 			</div>
-			<div class="col-md-3 col-xs-12 col-sm-3 text-center" style="padding: 2em;">
+			<div class="col-md-3 col-xs-12 col-sm-6 text-center" style="padding: 2em;">
 				<p><img src="<?php $image_path( 'icons/010-start.png' ) ?>" alt="" width="62" height="75"></p>
 				<h3 style="margin-top: 35px;">Energy</h3>
-				<p>Lead bleeding edge and then further your reach. Synchronizing below the line.</p>
+				<p class="">Lead bleeding edge and then further your reach. Synchronizing below the line.</p>
 			</div>
-			<div class="col-md-3 col-xs-12 col-sm-3 text-center" style="padding: 2em;">
+			<div class="col-md-3 col-xs-12 col-sm-6 text-center" style="padding: 2em;">
 				<p><img src="<?php $image_path( 'icons/011-develop.png' ) ?>" alt="" width="75" height="75"></p>
 				<h3 style="margin-top: 35px;">Success</h3>
-				<p>Lead bleeding edge and then further your reach. Synchronizing below the line.</p>
+				<p class="">Lead bleeding edge and then further your reach. Synchronizing below the line.</p>
 			</div>
 		</div>
 	</div>
