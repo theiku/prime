88b145980872a70b1318bb58571aef3197ff792f
Nicole <nicolepaschen42@gmail.com>
2018-10-23 14:41:29 -0400

    updateds starter post layout

--- partials/blog.php
+++ partials/blog.php
@@ -1,16 +1,41 @@
 <div class="boldgrid-section">
 	<div class="container">
+		<div class="row row-spacing-sm-bottom" style="padding-top: 15px;">
+			<div class="col-md-6 col-sm-12 col-xs-12">
+				<p class="h6 color1-color" style="margin-bottom: 0; text-transform: uppercase;">Take Your Steps</p>
+				<h1 class="h2" style="margin-top: 0;">Heading</h1>
+				<?php $divider(); ?>
+			</div>
+			<div class="col-md-6 col-sm-12 col-xs-12 align-column-center"></div>
+		</div>
+	</div>
+</div>
+<div class="boldgrid-section">
+	<div class="container">
+		<div class="row row-spacing-sm-bottom">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				<p class=""><img class="bg-img bg-img-3" src="<?php $image_path( 'blog-1.jpg' ) ?>"></p>
+				<p class="">Leverage integrated tech stacks and above all, build ROI. Targeting user engagement and try to further your reach. Building awareness while remembering to target the low hanging fruit. Growing brand ambassadors with a goal to maximise share of voice. Lead bleeding edge and then further your reach. Synchronizing below the line. Executing audience segments and try to be transparent. Generating analytics with a goal to build ROI. Grow stakeholder management so that we surprise and delight.</p>
+				<p class="">Leverage integrated tech stacks and above all, build ROI. Targeting user engagement and try to further your reach. Building awareness while remembering to target the low hanging fruit. Growing brand ambassadors with a goal to maximise share of voice. Lead bleeding edge and then further your reach. Synchronizing below the line. Executing audience segments and try to be transparent. Generating analytics with a goal to build ROI. Grow stakeholder management so that we surprise and delight.</p>
+				<p class="">Leverage integrated tech stacks and above all, build ROI. Targeting user engagement and try to further your reach. Building awareness while remembering to target the low hanging fruit. Growing brand ambassadors with a goal to maximise share of voice. Lead bleeding edge and then further your reach. Synchronizing below the line. Executing audience segments and try to be transparent. Generating analytics with a goal to build ROI. Grow stakeholder management so that we surprise and delight.</p>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="boldgrid-section">
+	<div class="container">
 		<div class="row">
-			<div class="col-md-12 col-xs-12 col-sm-12">
-				<h3>Plan Your Content</h3>
-				<p class="">If you're considering adding a blog to your site, you'll want to have a plan beforehand. Planning your blog will help your subject matter remain consistent over time. It'll also help you determine whether or not there's enough material to maintain a steady stream of posts.</p>
-				<p class="">One pitfall many new bloggers run into is starting a blog that isn't posted to frequently enough. A shortage of recent posts can give your visitors a bad impression of your business. One may think "I wonder if they’re still in business" or "they may want to hire a writer."</p>
-				<p class="">A blog, like any other customer facing aspect of your business, communicates your brand. If it isn't maintained and given proper attention, people will notice. Post regularly and keep your content fresh. Give your audience a reason to visit often.</p>
-				<p class="">
-				<!--more Read more &gt; -->
-				</p>
-				<h3>Find Your Audience</h3>
-				<p class="">While on the topic of audiences, you'll likely want to identify yours early on. If your blog is going to be set up to compliment a business, your target audience will likely be the same as your consumer base; you're then writing for the same people that buy your product. You'll want to allow any marketing material you've used inform the style and tone of your writing. Think of your blog as an extension of your company's brand. If, on the other hand, your business is completely new or you don't happen to be selling anything in particular, this is the time to start thinking about your brand.</p>
+			<div class="col-md-4 col-sm-4 col-xs-12 color4-background-color color-4-text-contrast bg-background-color" style="padding: 3em;">
+				<h3 class="" style="margin-top: 0;">Heading</h3>
+				<p class="">Leverage integrated tech stacks and above all, build ROI. Targeting user engagement and try to further your reach. Building awareness while remembering to target the low hanging fruit. Growing brand ambassadors with a goal to maximise share of voice.</p>
+			</div>
+			<div class="col-md-4 col-sm-4 col-xs-12 color3-background-color color-3-text-contrast bg-background-color" style="padding: 3em;">
+				<h3 class="" style="margin-top: 0;">Heading</h3>
+				<p class="">Leverage integrated tech stacks and above all, build ROI. Targeting user engagement and try to further your reach. Building awareness while remembering to target the low hanging fruit. Growing brand ambassadors with a goal to maximise share of voice.</p>
+			</div>
+			<div class="col-md-4 col-sm-4 col-xs-12 bg-background-color color-4-text-contrast color4-background-color" style="padding: 3em;">
+				<h3 class="" style="margin-top: 0;">Heading</h3>
+				<p class="">Leverage integrated tech stacks and above all, build ROI. Targeting user engagement and try to further your reach. Building awareness while remembering to target the low hanging fruit. Growing brand ambassadors with a goal to maximise share of voice.</p>
 			</div>
 		</div>
 	</div>
