74a1623fb8a2708d6024208b621283940079bcfb
Tim <dev@tim.ph>
2018-10-30 16:32:52 -0400

    update header/footer templates

--- templates/footer/footer.php
+++ templates/footer/footer.php
@@ -11,10 +11,7 @@
 <footer id="colophon" <?php BoldGrid::add_class( 'footer', [ 'site-footer' ] ); ?> role="contentinfo" <?php BoldGrid_Framework_Schema::footer( true ); ?>>
 	<?php do_action( 'boldgrid_footer_top' ); ?>
 	<div <?php BoldGrid::add_class( 'footer_content', [ 'footer-content' ] ); ?>>
-		<div <?php BoldGrid::add_class( 'social_menu_wrapper', [ 'social-menu-wrapper' ] ); ?>><?php do_action( 'boldgrid_menu_social' ); ?></div>
-		<div <?php BoldGrid::add_class( 'footer_center_menu_wrapper', [ 'footer-center-menu-wrapper' ] ); ?>><?php do_action( 'boldgrid_menu_footer_center' ); ?></div>
-		<div <?php BoldGrid::add_class( 'attribution_theme_mods_wrapper', [ 'attribution-theme-mods-wrapper' ] ); ?>><?php do_action( 'boldgrid_display_attribution_links' ); ?></div>
-		<div <?php BoldGrid::add_class( 'contact_block_wrapper', [ 'contact-block-wrapper' ] ); ?>><?php do_action( 'boldgrid_display_contact_block' ); ?></div>
+		<?php echo BoldGrid::dynamic_footer(); ?>
 	</div>
 	<?php do_action( 'boldgrid_footer_bottom' ); ?>
 </footer><!-- #colophon -->
--- templates/header/header.php
+++ templates/header/header.php
@@ -7,18 +7,12 @@
  * @package Prime
  */
 ?>
+
 <header id="masthead" <?php BoldGrid::add_class( 'header', [ 'header' ] ); ?> role="banner" <?php BoldGrid_Framework_Schema::header( true ); ?>>
+	<?php do_action( 'boldgrid_header_top' ); ?>
 	<div class="custom-header-media">
 		<?php the_custom_header_markup(); ?>
 	</div>
-	<div id="secondary-wrap" <?php BoldGrid::add_class( 'secondary_wrap', [ BoldGrid::get_container_classes( 'header' ) ] ); ?>>
-		<?php do_action( 'boldgrid_menu_secondary' ); ?>
-	</div>
-	<div id="navi-wrap" <?php BoldGrid::add_class( 'navi_wrap' ); ?>>
-		<div id="navi" <?php BoldGrid::add_class( 'navi' ); ?>>
-			<?php do_action( 'boldgrid_site_identity' ); ?>
-			<?php do_action( 'boldgrid_menu_main' ) ?>
-		</div>
-	</div>
+	<?php echo BoldGrid::dynamic_header(); ?>
 	<?php do_action( 'boldgrid_header_bottom' ); ?>
 </header><!-- #masthead -->
