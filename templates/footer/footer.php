ff2587552b2ee27596e6acc8868d52854787a720
Tim <dev@tim.ph>
2018-11-22 07:20:29 -0500

    adding prefixed class

--- templates/footer/footer.php
+++ templates/footer/footer.php
@@ -10,7 +10,7 @@
 ?>
 <footer id="colophon" <?php BoldGrid::add_class( 'footer', [ 'site-footer' ] ); ?> role="contentinfo" <?php BoldGrid_Framework_Schema::footer( true ); ?>>
 	<?php do_action( 'boldgrid_footer_top' ); ?>
-	<div <?php BoldGrid::add_class( 'footer_content', [ 'footer-content' ] ); ?>>
+	<div <?php BoldGrid::add_class( 'footer_content', [ 'bgtfw-footer', 'footer-content' ] ); ?>>
 		<?php echo BoldGrid::dynamic_footer(); ?>
 	</div>
 	<?php do_action( 'boldgrid_footer_bottom' ); ?>
