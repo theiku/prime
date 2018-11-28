5d2108d52a865700f9e331cabe1c14d8ad98673e
Rafael Ramos <rramo012@gmail.com>
2018-11-28 16:54:27 -0500

    Adding cover to HR

--- partials/utility.php
+++ partials/utility.php
@@ -13,7 +13,7 @@ $image_path = function ( $file ) {
 $divider = function() { ?>
 	<div class="row bg-editor-hr-wrap">
 		<div class="col-md-12 col-xs-12 col-sm-12">
-			<div>
+			<div class="bg-box bg-box-cover">
 				<hr class="bg-hr color1-color bg-hr-15" style="width: 50px; margin: 0px 80px 40px 0px; border-radius: 100px;">
 			</div>
 		</div>
