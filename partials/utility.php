4fb33e75e4340a920ecbdae9388281ce59255524
Rafael Ramos <rramo012@gmail.com>
2018-11-29 10:29:29 -0500

    Updating wraping class

--- partials/utility.php
+++ partials/utility.php
@@ -6,6 +6,7 @@
  *
  * @since 2.0.0
  */
+
 $image_path = function ( $file ) {
 	echo get_parent_theme_file_uri( 'images/' . $file );
 };
@@ -13,7 +14,7 @@ $image_path = function ( $file ) {
 $divider = function() { ?>
 	<div class="row bg-editor-hr-wrap">
 		<div class="col-md-12 col-xs-12 col-sm-12">
-			<div class="bg-box bg-box-cover">
+			<div class="bg-box-cover">
 				<hr class="bg-hr color1-color bg-hr-15" style="width: 50px; margin: 0px 80px 40px 0px; border-radius: 100px;">
 			</div>
 		</div>
