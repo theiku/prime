9e5c3ff336dc31b30a9796502709fe4c997a6c16
Tim <dev@tim.ph>
2019-03-01 16:49:54 -0500

    ensure framework is loaded before welcome stuff

--- functions.php
+++ functions.php
@@ -7,8 +7,10 @@
 
 locate_template( '/inc/boldgrid-theme-framework-config/config.php', true, true );
 
-require_once get_template_directory() . '/inc/class-boldgrid-crio-welcome.php';
-$welcome = new BoldGrid_Crio_Welcome();
-$welcome->add_hooks();
-
 require_once get_template_directory() . '/inc/boldgrid-theme-framework/boldgrid-theme-framework.php';
+
+if ( class_exists( 'Boldgrid_Framework' ) ) {
+	require_once get_template_directory() . '/inc/class-boldgrid-crio-welcome.php';
+	$welcome = new BoldGrid_Crio_Welcome();
+	$welcome->add_hooks();
+}
