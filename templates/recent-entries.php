40f4125955fef83b9ed461a79398cf0827c18da5
Tim <dev@tim.ph>
2019-03-16 16:57:51 -0400

    escape recent entries template

--- templates/recent-entries.php
+++ templates/recent-entries.php
@@ -17,14 +17,14 @@
 	<div class="row">
 		<?php if ( $bgtfw_recent_posts ) : ?>
 			<div class="<?php echo ! $bgtfw_pages ? 'col-md-12' : 'col-md-6'; ?> search-posts">
-				<h3><?php _e( 'Recent Articles', 'bgtfw' ); ?></h3>
+				<h3><?php esc_html_e( 'Recent Articles', 'bgtfw' ); ?></h3>
 				<ul class="list-group">
 					<?php
 						$bgtfw_args = array( 'numberposts' => '10', 'post_status' => 'publish' );
 						$bgtfw_recent_posts = wp_get_recent_posts( $bgtfw_args );
 					foreach ( $bgtfw_recent_posts as $bgtfw_recent ) {
 						if ( $bgtfw_recent['post_title'] ) {
-							echo '<li class="list-group-item"><a href="' . get_permalink( $bgtfw_recent['ID'] ) . '">' . $bgtfw_recent['post_title'] . '</a></li>';
+							echo '<li class="list-group-item"><a href="' . esc_url( get_permalink( $bgtfw_recent['ID'] ) ) . '">' . wp_kses_post( $bgtfw_recent['post_title'] ) . '</a></li>';
 						}
 					}
 					?>
@@ -33,13 +33,13 @@
 		<?php endif; ?>
 		<?php if ( $bgtfw_pages ) : ?>
 			<div class="<?php echo ! $bgtfw_recent_posts ? 'col-md-12' : 'col-md-6'; ?> search-pages">
-				<h3><?php _e( 'Page Sitemap', 'bgtfw' ); ?></h3>
+				<h3><?php esc_html_e( 'Page Sitemap', 'bgtfw' ); ?></h3>
 				<ul class="list-group">
 					<?php
 						$count = 0;
 					foreach ( $bgtfw_pages as $bgtfw_page ) {
 						if ( $bgtfw_page->post_title && $count < 10 ) {
-							echo '<li class="list-group-item"><a href="' . get_permalink( $bgtfw_page->ID ) . '">' . $bgtfw_page->post_title . '</a></li>';
+							echo '<li class="list-group-item"><a href="' . esc_url( get_permalink( $bgtfw_page->ID ) ) . '">' . wp_kses_post( $bgtfw_page->post_title ) . '</a></li>';
 						}
 						$count++;
 					}
