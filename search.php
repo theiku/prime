828af983f57edf905d9c4914b8937b96f5c661f8
Tim <dev@tim.ph>
2019-03-16 16:39:47 -0400

    ensure escaped output on search page

--- search.php
+++ search.php
@@ -12,7 +12,7 @@ $results_count = $wp_query->found_posts;
 		<div class="container">
 			<h1>Search <span class="keyword">&ldquo;<?php the_search_query(); ?>&rdquo;</span></h1>
 			<?php if ( '' == $results_count || 0 == $results_count ) { // No Results ?>
-				<p><span class="label label-danger"><?php _e( 'No Results', 'bgtfw' ); ?></span>&nbsp; <?php _e( 'Try different search terms.', 'bgtfw' ); ?></p>
+				<p><span class="label label-danger"><?php esc_html_e( 'No Results', 'bgtfw' ); ?></span>&nbsp; <?php esc_html_e( 'Try different search terms.', 'bgtfw' ); ?></p>
 			<?php } else { // Results Found. ?>
 				<p><span class="label label-success"><?php echo absint( $results_count ) . ' ' . esc_html__( 'Results', 'bgtfw' ); ?></span></p>
 			<?php } // End results check. ?>
@@ -27,12 +27,12 @@ $results_count = $wp_query->found_posts;
 		<div class="row">
 			<div class="<?php echo BoldGrid::display_sidebar() ? 'col-md-9' : 'col-md-12'; ?>">
 				<?php if ( have_posts() ) : // Results Found. ?>
-					<h1><?php _e( 'Search Results', 'bgtfw' ); ?></h1>
+					<h1><?php esc_html_e( 'Search Results', 'bgtfw' ); ?></h1>
 					<?php while ( have_posts() ) : the_post(); ?>
 					<article <?php post_class(); ?>>
 						<h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 						<div class="entry">
-							<p><?php echo wp_trim_words( get_the_excerpt(), 30, '...' ); ?></p>
+							<p><?php echo esc_html( wp_trim_words( get_the_excerpt(), 30, '...' ) ); ?></p>
 						</div>
 					</article>
 					<hr />
@@ -43,7 +43,7 @@ $results_count = $wp_query->found_posts;
 		<?php else : // No Results. ?>
 		<div class="row">
 			<div class="<?php echo BoldGrid::display_sidebar( ) ? 'col-md-9' : 'col-md-12'; ?>">
-				<p><?php _e( 'Sorry. We couldn&rsquo;t find anything for that search. View one of our site&rsquo;s pages or a recent article below.', 'bgtfw' ); ?></p>
+				<p><?php esc_html_e( 'Sorry. We couldn&rsquo;t find anything for that search. View one of our site&rsquo;s pages or a recent article below.', 'bgtfw' ); ?></p>
 			</div><!-- .col-md-12 -->
 		</div> <!-- .row -->
 		<?php get_template_part( 'templates/recent-entries' ); ?>
