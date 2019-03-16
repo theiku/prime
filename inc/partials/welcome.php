fe3a2146175f3d705dff724f4fcb89cb28399af6
Tim <dev@tim.ph>
2019-03-16 17:03:14 -0400

    fix escaping on welcome page

--- inc/partials/welcome.php
+++ inc/partials/welcome.php
@@ -48,7 +48,7 @@ $configs = $boldgrid_theme_framework->get_configs();
 					<div class="welcome-panel-column">
 						<h2><?php esc_html_e( 'Getting Started', 'bgtfw' ); ?></h2>
 						<p>
-							<?php _e( 'Welcome to BoldGrid Crio! In order to give you a head start editing and designing, we have installed Starter Content for you. You may edit any part of the content to suit your needs or delete content and pages you don\'t find valuable.  Our Starter Content works best with the <a href="https://wordpress.org/plugins/post-and-page-builder/" target="_blank">Post and Page Builder</a> by <a href="https://www.boldgrid.com/" target="_blank">Boldgrid</a>. Click below to install.', 'bgtfw' ); ?>
+							<?php wp_kses_post( _e( 'Welcome to BoldGrid Crio! In order to give you a head start editing and designing, we have installed Starter Content for you. You may edit any part of the content to suit your needs or delete content and pages you don\'t find valuable.  Our Starter Content works best with the <a href="https://wordpress.org/plugins/post-and-page-builder/" target="_blank">Post and Page Builder</a> by <a href="https://www.boldgrid.com/" target="_blank">Boldgrid</a>. Click below to install.', 'bgtfw' ) ); ?>
 						</p>
 						<p>
 							<a href="<?php echo esc_url( $tgm_url ) ?>" class="button button-primary button-hero"><?php esc_html_e( 'Install Post and Page Builder', 'bgtfw' ); ?></a>
@@ -58,19 +58,19 @@ $configs = $boldgrid_theme_framework->get_configs();
 						</p>
 					</div>
 					<div class="welcome-panel-column">
-						<img style="width:100%;" src="<?php echo get_template_directory_uri() . '/images/welcome/bg-fresh-site.jpg'; ?>" />
+						<img style="width:100%;" src="<?php echo esc_url( get_template_directory_uri() ) . '/images/welcome/bg-fresh-site.jpg'; ?>" />
 					</div>
 				<?php else : ?>
 					<div class="welcome-panel-column">
 						<h2><?php esc_html_e( 'Crio - The theme with more', 'bgtfw' ); ?></h2>
 						<p>
-							<?php _e( 'Crio means "I Create" in Portuguese and this is our aim: To give you the most powerful site creation tools you can use! Visit <a href="https://www.BoldGrid.com/" target="_blank">BoldGrid.com</a> to learn about all the resources we offer, including our new WordPress Cloud toolset where you can create in seconds!', 'bgtfw' ); ?></p>
+							<?php wp_kses_post( _e( 'Crio means "I Create" in Portuguese and this is our aim: To give you the most powerful site creation tools you can use! Visit <a href="https://www.BoldGrid.com/" target="_blank">BoldGrid.com</a> to learn about all the resources we offer, including our new WordPress Cloud toolset where you can create in seconds!', 'bgtfw' ) ); ?></p>
 						<p>
 							<a href="https://www.boldgrid.com/" target="_blank"><?php esc_html_e( 'BoldGrid.com', 'bgtfw' ); ?></a>
 						</p>
 					</div>
 					<div class="welcome-panel-column">
-						<img style="width:100%;" src="<?php echo get_template_directory_uri() . '/images/welcome/bg-no-fresh-site.png'; ?>" />
+						<img style="width:100%;" src="<?php echo esc_url( get_template_directory_uri() ) . '/images/welcome/bg-no-fresh-site.png'; ?>" />
 					</div>
 				<?php endif; ?>
 			</div>
@@ -131,7 +131,7 @@ $configs = $boldgrid_theme_framework->get_configs();
 					</p>
 				</div>
 				<div class="welcome-panel-column">
-					<img style="width:100%;" src="<?php echo get_template_directory_uri() . '/images/welcome/bg-central.png'; ?>" />
+					<img style="width:100%;" src="<?php echo esc_url( get_template_directory_uri() ) . '/images/welcome/bg-central.png'; ?>" />
 				</div>
 			</div>
 		</div>
