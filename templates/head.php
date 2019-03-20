8cb7930711dcb09a4bcaf96c52022c376c87433a
Tim <dev@tim.ph>
2019-03-20 02:50:37 -0400

    move x-ua-compat to first item

--- templates/head.php
+++ templates/head.php
@@ -6,12 +6,12 @@
  * a prime theme.
  *
  * @package Prime
+ * @license GPL-3.0-or-later
  */
 ?>
-
 <head>
-	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<?php wp_head(); ?>
 </head>
