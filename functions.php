f0e98409c489b776d39ef3fbd0721013a2d11e36
Tim <dev@tim.ph>
2019-03-28 09:19:38 -0400

    prefix global

--- functions.php
+++ functions.php
@@ -11,6 +11,6 @@ require_once get_template_directory() . '/inc/boldgrid-theme-framework/boldgrid-
 
 if ( class_exists( 'Boldgrid_Framework' ) ) {
 	require_once get_template_directory() . '/inc/class-boldgrid-crio-welcome.php';
-	$welcome = new BoldGrid_Crio_Welcome();
-	$welcome->add_hooks();
+	$crio_welcome = new BoldGrid_Crio_Welcome();
+	$crio_welcome->add_hooks();
 }
