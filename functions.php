bbc5a9e9923ec6a53a82e831e2f11b63e5a4d801
Tim <dev@tim.ph>
2019-03-28 15:26:22 -0400

    leading slash optional

--- functions.php
+++ functions.php
@@ -5,11 +5,11 @@
  * @package Prime
  */
 
-require_once get_theme_file_path( '/inc/boldgrid-theme-framework-config/config.php' );
-require_once get_parent_theme_file_path( '/inc/boldgrid-theme-framework/boldgrid-theme-framework.php' );
+require_once get_theme_file_path( 'inc/boldgrid-theme-framework-config/config.php' );
+require_once get_parent_theme_file_path( 'inc/boldgrid-theme-framework/boldgrid-theme-framework.php' );
 
 if ( class_exists( 'Boldgrid_Framework' ) ) {
-	require_once get_parent_theme_file_path( '/inc/class-boldgrid-crio-welcome.php' );
+	require_once get_parent_theme_file_path( 'inc/class-boldgrid-crio-welcome.php' );
 	$crio_welcome = new BoldGrid_Crio_Welcome();
 	$crio_welcome->add_hooks();
 }
