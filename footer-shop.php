4026924382ce82b48855fd27accc3f2dbe759556
Tim <dev@tim.ph>
2019-07-11 15:53:15 -0400

    update inline docs for header/footer shop template files

--- footer-shop.php
+++ footer-shop.php
@@ -1,6 +1,9 @@
 <?php
 /**
- * Included for WooCommerce overrides.
+ * This file is included for WooCommerce overrides.
+ *
+ * This is left intentionally empty, and loaded so WooCommerce doesn't have any errors and
+ * only a single footer section is displayed on the site.
  *
  * @package Prime
  */
