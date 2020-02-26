11168a4ea7dce811be98857e6a4df57a6fd0eb73
joe9663 <joe9663@gmail.com>
2020-02-26 14:04:57 -0500

    adds quantity input buttons

--- woocommerce/global/quantity-input.php
+++ woocommerce/global/quantity-input.php
@@ -30,10 +30,15 @@ if ( $max_value && $min_value === $max_value ) {
 	<div class="quantity input-group">
 		<?php do_action( 'woocommerce_before_quantity_input_field' ); ?>
 		<label class="screen-reader-text" for="<?php echo esc_attr( $input_id ); ?>"><?php echo esc_attr( $label ); ?></label>
+        <span class="input-group-btn">
+			<button type="button" class="btn btn-minus btn-number"  data-type="minus" data-field="<?php echo esc_attr( $input_name ); ?>" disabled="disabled">
+				<span class="fa fa-minus"></span>
+			</button>
+		</span>
 		<input
 			type="number"
 			id="<?php echo esc_attr( $input_id ); ?>"
-			class="<?php echo esc_attr( join( ' ', (array) $classes ) ); ?>"
+			class="<?php echo esc_attr( join( ' ', (array) $classes ) ); ?> form-control input-number"
 			step="<?php echo esc_attr( $step ); ?>"
 			min="<?php echo esc_attr( $min_value ); ?>"
 			max="<?php echo esc_attr( 0 < $max_value ? $max_value : '' ); ?>"
@@ -43,6 +48,11 @@ if ( $max_value && $min_value === $max_value ) {
 			size="4"
 			placeholder="<?php echo esc_attr( $placeholder ); ?>"
 			inputmode="<?php echo esc_attr( $inputmode ); ?>" />
+            <span class="input-group-btn">
+			<button type="button" class="btn btn-plus btn-number" data-type="plus" data-field="<?php echo esc_attr( $input_name ); ?>">
+				<span class="fa fa-plus"></span>
+			</button>
+		</span>
 		<?php do_action( 'woocommerce_after_quantity_input_field' ); ?>
 	</div>
 	<?php
