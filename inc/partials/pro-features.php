6888a29f3d588c2593e621ffdf168ba2a859a3e2
James Rosado <jamesros@boldgrid.com>
2021-01-14 11:38:16 -0500

    fix logo issues

--- inc/partials/pro-features.php
+++ inc/partials/pro-features.php
@@ -15,7 +15,7 @@ if ( ! defined( 'WPINC' ) ) {
  * it's function is defined in the class
  * Boldgrid_Framework_Pro_Feature_Cards.
  */
-$go_pro_url = apply_filters( 'bgtfw_upgrade_url_pro_features' );
+$go_pro_url = 'https://www.boldgrid.com/central/checkout/crio?utm_source=Crio_-_Pro_Features&utm_medium=Button&utm_campaign=Crio_Pro_Features';
 
 ?>
 
@@ -28,7 +28,7 @@ $go_pro_url = apply_filters( 'bgtfw_upgrade_url_pro_features' );
 	</div>
 	<div class="pro-feature-image" style="background-image:url(<?php echo esc_url( get_template_directory_uri() ) . '/images/welcome/custom-page-headers.png'; ?>)">
 		<h2 class="cta-heading">Crio Pro</h2>
-		<a class="cta-button" href="<?php echo esc_url( $go_pro_url ); ?>&utm_content=Go_Pro " target="_blank">Go Pro</a>
+		<a class="cta-button" href="<?php echo esc_url( $go_pro_url ); ?>&utm_content=Upgrade_Now " target="_blank">Upgrade Now</a>
 	</div>
 <?php
 /**
